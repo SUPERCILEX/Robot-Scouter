a145d24e0976e8fe512c782f5059c593123532a0
Alex Saveau <gojunkmailx@gmail.com>
2016-09-25 19:37:51 -0700

    Auth flow refactor. Waiting for https://github.com/firebase/FirebaseUI-Android/pull/309.

--- app/src/main/java/com/supercilex/robotscouter/MainActivity.java
+++ app/src/main/java/com/supercilex/robotscouter/MainActivity.java
@@ -15,25 +15,13 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.supercilex.robotscouter.dialogfragments.CreateNewTeamDialogFragment;
 import com.supercilex.robotscouter.models.team.Team;
 import com.supercilex.robotscouter.models.team.TeamHolder;
@@ -59,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
     private Bundle mSavedState;
     private Toolbar mToolbar;
-    private Drawer mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,18 +75,23 @@ public class MainActivity extends AppCompatActivity {
                 if (mFirebaseUser != null) {
                     attachRecyclerViewAdapter();
                 } else {
+                    mAuth.signInAnonymously()
+                            .addOnFailureListener(MainActivity.this, new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // TODO: 09/24/2016 retry
+                                    Snackbar.make(findViewById(android.R.id.content),
+                                                  "Sign In Failed",
+                                                  Snackbar.LENGTH_SHORT)
+                                            .show();
+                                    FirebaseCrash.report(e);
+                                }
+                            });
                     // TODO show a tutorial (pretend first time app start)
                 }
-                initializeDrawer();
             }
         };
         mAuth.addAuthStateListener(mAuthStateListener);
-
-//        if (!isSignedIn()) {
-//            signInAnonymously();
-//        } else {
-//            attachRecyclerViewAdapter();
-//        }
 
 //        LeakCanary.install(getApplication());
 
@@ -134,11 +126,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (mDrawer.isDrawerOpen()) {
-            mDrawer.closeDrawer();
-        } else {
+//        if (mDrawer.isDrawerOpen()) {
+//            mDrawer.closeDrawer();
+//        } else {
             super.onBackPressed();
-        }
+//        }
     }
 
     @Override
@@ -155,8 +147,18 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+//            AuthUI.getInstance().signOut(MainActivity.this);
+            // TODO: 09/06/2016 Fix sign in
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setLogo(R.drawable.launch_logo_image)
+                            .setProviders(AuthUI.EMAIL_PROVIDER,
+                                          AuthUI.GOOGLE_PROVIDER,
+                                          AuthUI.FACEBOOK_PROVIDER)
+                            .setTosUrl("example.com")
+                            .build(),
+                    RC_SIGN_IN);
             return true;
         }
 
@@ -234,127 +236,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-    }
-
-    // TODO: 08/29/2016 move all old uid to new uid and delete old https://github.com/firebase/FirebaseUI-Android/issues/123
-    private void signInAnonymously() {
-        mAuth.signInAnonymously()
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            attachRecyclerViewAdapter();
-                            initializeDrawer();
-                        } else {
-                            Snackbar.make(findViewById(android.R.id.content),
-                                          "Sign In Failed",
-                                          Snackbar.LENGTH_SHORT)
-                                    .show();
-                            FirebaseCrash.report(task.getException());
-                        }
-                    }
-                });
-    }
-
-    private boolean isSignedIn() {
-        return (mAuth.getCurrentUser() != null);
-    }
-
-    // TODO: 08/04/2016 get rid of this
-    private void initializeDrawer() {
-        AccountHeader header;
-        if (isSignedIn())
-            if (!mAuth.getCurrentUser().isAnonymous()) {
-                // already signed in
-                FirebaseUser user = Utils.getUser();
-                header = new AccountHeaderBuilder()
-                        .withActivity(this)
-                        .withHeaderBackground(R.mipmap.ic_launcher)
-                        .withSelectionListEnabledForSingleProfile(false)
-                        .addProfiles(
-                                new ProfileDrawerItem().withName(user.getDisplayName())
-                                        .withEmail(user.getEmail())
-                                        .withIcon(R.drawable.ic_person_black_24dp)
-                        )
-                        .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                            @Override
-                            public boolean onProfileChanged(View view,
-                                                            IProfile profile,
-                                                            boolean currentProfile) {
-                                AuthUI.getInstance()
-                                        .signOut(MainActivity.this)
-                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                // user is now signed out
-                                            }
-                                        });
-                                return true;
-                            }
-                        })
-                        .build();
-            } else {
-                header = getNavBarSignedOut();
-            }
-        else {
-            // not signed in
-            header = getNavBarSignedOut();
-        }
-
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1)
-                .withName("R.string.drawer_item_home");
-        mDrawer = new DrawerBuilder()
-                .withActivity(this)
-                .withToolbar(mToolbar)
-                .withAccountHeader(header)
-                .addDrawerItems(
-                        item1,
-                        new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withIdentifier(2)
-                                .withName("R.string.drawer_item_settings1"),
-                        new SecondaryDrawerItem().withName("R.string.drawer_item_setting2s")
-                )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        // do something with the clicked item :D
-                        return true;
-                    }
-                })
-                .build();
-
-        mDrawer.setSelection(item1);
-    }
-
-    private AccountHeader getNavBarSignedOut() {
-        AccountHeader header;// not signed in
-        header = new AccountHeaderBuilder()
-                .withActivity(this)
-                .withHeaderBackground(R.mipmap.ic_launcher)
-                .withSelectionListEnabledForSingleProfile(false)
-                .addProfiles(
-                        new ProfileDrawerItem().withName(getString(R.string.sign_in))
-                                .withIcon(R.drawable.ic_person_black_24dp)
-                )
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view,
-                                                    IProfile profile,
-                                                    boolean currentProfile) {
-                        // TODO: 09/06/2016 Fix sign in
-                        startActivityForResult(
-                                AuthUI.getInstance().createSignInIntentBuilder()
-                                        .setLogo(R.drawable.launch_logo_image)
-                                        .setProviders(AuthUI.EMAIL_PROVIDER,
-                                                      AuthUI.GOOGLE_PROVIDER,
-                                                      AuthUI.FACEBOOK_PROVIDER)
-                                        .setTosUrl("example.com")
-                                        .build(),
-                                RC_SIGN_IN);
-                        return true;
-                    }
-                })
-                .build();
-        return header;
     }
 }
