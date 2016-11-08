0dad1926aaea5d33219e81823deeadd6e7ea34cb
Alex Saveau <SUPERCILEX@users.noreply.github.com>
2016-11-08 15:03:32 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -v
+
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   ./gradlew clean build publishApkRelease
 else
   ./gradlew clean build
 fi
+
+set +v
