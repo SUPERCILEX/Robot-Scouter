acdbeb5b920e2d3593541974863dff5f054cddb8
Alex Saveau <gojunkmailx@gmail.com>
2016-11-08 00:15:45 -0800

    Fix Travis

--- upload.sh
+++ upload.sh
@@ -2,20 +2,23 @@
 
 if [[ $TRAVIS_PULL_REQUEST = "false" ]]; then
   mv app/build/outputs/apk/app-release.apk app-release.apk
-  mv app/build/outputs/mapping/release/mapping.txt mapping.txt
 
   wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-133.0.0-linux-x86_64.tar.gz
   tar xf google-cloud-sdk-133.0.0-linux-x86_64.tar.gz
   echo "y" | ./google-cloud-sdk/bin/gcloud components update alpha
   ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file app/google-play-auto-publisher.json
 
-  git init
-  git remote add mapping $git_mapping_login
+  cd ..
+  git clone --branch=master $git_mapping_login uploads
+  mv Robot-Scouter/app-release.apk uploads/app-release.apk
+  mv Robot-Scouter/app/build/outputs/mapping/release/mapping.txt uploads/mapping.txt
+  cd uploads
   git add mapping.txt app-release.apk
-  git config --global user.email $github_email
-  git config --global user.name "Alexandre Saveau"
+#  git config --global user.email $github_email
+#  git config --global user.name "Alexandre Saveau"
   git commit -a -m "Added new apk + mapping"
-  git push -u mapping master &> /dev/null
+  git push -u origin master &> /dev/null
+  cd ..
 
   ./google-cloud-sdk/bin/gcloud alpha test android run --async --app app-release.apk --device-ids m0,Nexus6P --os-version-ids 18,25 --orientations portrait --project robot-scouter-app
 fi
