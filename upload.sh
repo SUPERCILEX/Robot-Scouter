6f625ec61163a09ab89880af8b94c474cbd94d92
Alex Saveau <gojunkmailx@gmail.com>
2016-11-08 19:34:13 -0800

    Speed up Travis for not master pushes

--- upload.sh
+++ upload.sh
@@ -11,8 +11,8 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   mv Robot-Scouter/app/build/outputs/mapping/release/mapping.txt uploads/mapping.txt
   cd uploads
 
-  APK_INFO=$(/usr/local/android-sdk/build-tools/25.0.0/aapt dump badging app-release.apk)
-  VERSION_CODE=$(echo $APK_INFO | grep 'versionCode=' | awk -F: 'match($0,"versionCode="){ print substr($2,37,19)}' | tr -d "'")
+  APK_INFO=$(/usr/local/android-sdk/build-tools/25.0.0/aapt dump badging app-release.apk) &> /dev/null
+  VERSION_CODE=$(echo $APK_INFO | grep 'versionCode=' | awk -F: 'match($0,"versionCode="){ print substr($2,37,19)}' | tr -d "'") &> /dev/null
   git add mapping.txt app-release.apk
   git commit -a -m "$(printf "${VERSION_CODE}\n Full apk dump: ${APK_INFO}")"
   git push -u origin master &> /dev/null
