a94b8a56816cd3530e1110e8d24774b4fb21eeee
Alex Saveau <saveau.alexandre@gmail.com>
2016-12-18 23:40:19 -0800

    Fix Travis displaying github api key
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   mv app/build/outputs/apk/app-prod-release.apk app-release.apk
@@ -8,10 +8,10 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   git config --global user.name "Alexandre Saveau"
 
   cp Robot-Scouter/app-release.apk uploads/app-release.apk
-  mv Robot-Scouter/app/build/outputs/mapping/release/mapping.txt uploads/mapping.txt
+  mv Robot-Scouter/app/build/outputs/mapping/prod/release/mapping.txt uploads/mapping.txt
   cd uploads
 
-  APK_INFO=$(/usr/local/android-sdk/build-tools/25.0.1/aapt dump badging app-release.apk) &> /dev/null
+  APK_INFO=$(/usr/local/android-sdk/build-tools/25.0.2/aapt dump badging app-release.apk) &> /dev/null
   VERSION_CODE=$(echo $APK_INFO | grep 'versionCode=' | awk -F: 'match($0,"versionCode="){ print substr($2,37,19)}' | tr -d "'") &> /dev/null
   git add mapping.txt app-release.apk
   git commit -a -m "$(printf "${VERSION_CODE}\n Full apk dump: ${APK_INFO}")"
@@ -26,5 +26,3 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
 #  ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file app/google-play-auto-publisher.json
 #  ./google-cloud-sdk/bin/gcloud alpha test android run --async --app app-release.apk --device-ids m0,Nexus6P --os-version-ids 18,25 --orientations portrait --project robot-scouter-app
 fi
-
-set +xe
