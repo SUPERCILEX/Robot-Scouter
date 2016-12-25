f78ec97ea23c09a5d880976fc90757acf5a0a73b
Alexandre Saveau <saveau.alexandre@gmail.com>
2016-12-25 00:35:19 -0800

    Ignore network timeouts
    Signed-off-by: Alexandre Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/util/TaskFailureLogger.java
+++ app/src/main/java/com/supercilex/robotscouter/util/TaskFailureLogger.java
@@ -6,12 +6,15 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.crash.FirebaseCrash;
 
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 
 public class TaskFailureLogger implements OnFailureListener {
     @Override
     public void onFailure(@NonNull Exception e) {
-        if (!(e instanceof FirebaseNetworkException) && !(e instanceof UnknownHostException)) {
+        if (!(e instanceof FirebaseNetworkException)
+                && !(e instanceof UnknownHostException)
+                && !(e instanceof SocketTimeoutException)) {
             FirebaseCrash.report(e);
         }
     }
