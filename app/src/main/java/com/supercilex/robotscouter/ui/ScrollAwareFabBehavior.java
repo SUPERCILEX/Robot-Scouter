0b7be16c8ac4ce8d2158e498d72cd551a23cfed0
Alex Saveau <saveau.alexandre@gmail.com>
2016-12-30 17:52:59 -0800

    Add hack to fix FAB not coming back after scroll
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/ui/ScrollAwareFabBehavior.java
+++ app/src/main/java/com/supercilex/robotscouter/ui/ScrollAwareFabBehavior.java
@@ -14,27 +14,28 @@ public class ScrollAwareFabBehavior extends FloatingActionButton.Behavior {
     }
 
     @Override
-    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout,
-                                       final FloatingActionButton child,
-                                       final View directTargetChild,
-                                       final View target,
-                                       final int nestedScrollAxes) {
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
-                || super.onStartNestedScroll(coordinatorLayout,
-                                             child,
-                                             directTargetChild,
-                                             target,
-                                             nestedScrollAxes);
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+                                       FloatingActionButton child,
+                                       View directTargetChild,
+                                       View target,
+                                       int nestedScrollAxes) {
+        // Ensure we react to vertical scrolling
+        return super.onStartNestedScroll(coordinatorLayout,
+                                         child,
+                                         directTargetChild,
+                                         target,
+                                         nestedScrollAxes)
+                || nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
 
     @Override
-    public void onNestedScroll(final CoordinatorLayout coordinatorLayout,
-                               final FloatingActionButton child,
-                               final View target,
-                               final int dxConsumed,
-                               final int dyConsumed,
-                               final int dxUnconsumed,
-                               final int dyUnconsumed) {
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout,
+                               FloatingActionButton child,
+                               View target,
+                               int dxConsumed,
+                               int dyConsumed,
+                               int dxUnconsumed,
+                               int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout,
                              child,
                              target,
@@ -45,7 +46,14 @@ public class ScrollAwareFabBehavior extends FloatingActionButton.Behavior {
 
         if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
             // User scrolled down and the FAB is currently visible -> hide the FAB
-            child.hide();
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    // TODO remove hacky listener in support lib 25.1.1
+                    // See: http://stackoverflow.com/a/41150767/4548500
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
         } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
             // User scrolled up and the FAB is currently not visible -> show the FAB
             child.show();
