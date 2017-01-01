121de958363f7b7a8011857e777d46d733bfabe0
Alex Saveau <saveau.alexandre@gmail.com>
2017-01-01 12:29:23 -0800

    Add testing again
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   cd ..
   git clone --branch=master "https://SUPERCILEX:$git_mapping_login@github.com/SUPERCILEX/mapping.txt-files.git" uploads &> /dev/null
   git config --global user.email "saveau.alexandre@gmail.com"
-  git config --global user.name "Alexandre Saveau"
+  git config --global user.name "Alex Saveau"
 
   cp Robot-Scouter/app-release.apk uploads/app-release.apk
   mv Robot-Scouter/app/build/outputs/mapping/prod/release/mapping.txt uploads/mapping.txt
@@ -20,9 +20,9 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   cd ..
   cd Robot-Scouter
 
-#  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-136.0.0-linux-x86_64.tar.gz
-#  tar xf google-cloud-sdk-136.0.0-linux-x86_64.tar.gz
-#  echo "y" | ./google-cloud-sdk/bin/gcloud components update alpha
-#  ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file app/google-play-auto-publisher.json
-#  ./google-cloud-sdk/bin/gcloud alpha test android run --async --app app-release.apk --device-ids m0,Nexus6P --os-version-ids 18,25 --orientations portrait --project robot-scouter-app
+  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-138.0.0-linux-x86_64.tar.gz
+  tar xf google-cloud-sdk-138.0.0-linux-x86_64.tar.gz
+  echo "y" | ./google-cloud-sdk/bin/gcloud components update alpha
+  ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file app/google-play-auto-publisher.json
+  ./google-cloud-sdk/bin/gcloud alpha test android run --async --app app-release.apk --device-ids m0,Nexus6P --os-version-ids 18,25 --orientations portrait --project robot-scouter-app
 fi
