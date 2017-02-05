b8050ea6415120806760ef646fce51ec558dbce3
Alex Saveau <saveau.alexandre@gmail.com>
2017-02-04 16:17:26 -0800

    #64
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/ui/teamlist/LicensesDialog.java
+++ app/src/main/java/com/supercilex/robotscouter/ui/teamlist/LicensesDialog.java
@@ -31,26 +31,27 @@ public class LicensesDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View rootView = View.inflate(getContext(), R.layout.recycler_view, null);
 
-        List<LicenseEntry> licenses = new ArrayList<>(); // TODO check that I have them all
+        List<LicenseEntry> licenses = new ArrayList<>();
         // TODO fix licenses once new repo version comes out
 
         licenses.add(Licenses.noContent("Firebase",
                                         "Google Inc.",
                                         "https://firebase.google.com/terms/"));
-//        licenses.add(Licenses.noContent("Google Play Services",
-//                                        "Google Inc.",
-//                                        GoogleApiAvailability.getInstance()
-//                                                .getOpenSourceSoftwareLicenseInfo(getContext())));
-        licenses.add(Licenses.fromGitHub("firebase/FirebaseUI-Android",
-                                         Licenses.LICENSE_APACHE_V2));
-        licenses.add(Licenses.fromGitHub("firebase/firebase-jobdispatcher-android",
-                                         Licenses.LICENSE_APACHE_V2));
-        licenses.add(Licenses.fromGitHub("square/retrofit", Licenses.LICENSE_APACHE_V2));
-        licenses.add(Licenses.fromGitHub("bumptech/glide", "Glide license", "LICENSE"));
-        licenses.add(Licenses.fromGitHub("hdodenhof/CircleImageView", Licenses.LICENSE_APACHE_V2));
+//        licenses.add(Licenses.noLink("Google Play Services",
+//                                     "Google Inc.",
+//                                     GoogleApiAvailability.getInstance()
+//                                             .getOpenSourceSoftwareLicenseInfo(getContext())));
+        licenses.add(Licenses.fromGitHubApacheV2("firebase/FirebaseUI-Android"));
+        licenses.add(Licenses.fromGitHubApacheV2("firebase/firebase-jobdispatcher-android"));
+        licenses.add(Licenses.fromGitHubApacheV2("googlesamples/easypermissions"));
+//        licenses.add(Licenses.fromGitHub("bumptech/glide", "Glide license", Licenses.FILE_AUTO));
+        licenses.add(Licenses.fromGitHubApacheV2("hdodenhof/CircleImageView"));
+        licenses.add(Licenses.fromGitHubApacheV2("Clans/FloatingActionButton"));
+        licenses.add(Licenses.fromGitHubApacheV2("sjwall/MaterialTapTargetPrompt"));
+        licenses.add(Licenses.fromGitHubApacheV2("square/retrofit"));
+        licenses.add(Licenses.fromGitHubApacheV2("square/leakcanary"));
         licenses.add(Licenses.fromGitHubMIT("Triple-T/gradle-play-publisher"));
-        licenses.add(Licenses.fromGitHub("square/leakcanary", Licenses.LICENSE_APACHE_V2));
-        licenses.add(Licenses.fromGitHub("yshrsmz/LicenseAdapter", Licenses.LICENSE_APACHE_V2));
+        licenses.add(Licenses.fromGitHubApacheV2("yshrsmz/LicenseAdapter"));
 
         RecyclerView list = (RecyclerView) rootView.findViewById(R.id.list);
         list.setLayoutManager(new LinearLayoutManager(getContext()));
