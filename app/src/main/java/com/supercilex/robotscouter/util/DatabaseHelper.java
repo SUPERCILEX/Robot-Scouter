08ca0ed3b66a36878c5f5e34991d49ddf1c33b8b
Alex Saveau <saveau.alexandre@gmail.com>
2017-02-04 16:00:04 -0800

    Add versioning to Database
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/util/DatabaseHelper.java
+++ app/src/main/java/com/supercilex/robotscouter/util/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.supercilex.robotscouter.util;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.supercilex.robotscouter.BuildConfig;
 
 public final class DatabaseHelper {
     private DatabaseHelper() {
@@ -9,14 +10,18 @@ public final class DatabaseHelper {
     }
 
     public static DatabaseReference getReference() {
-        return DatabaseHolder.INSTANCE.getReference();
+        return DatabaseHolder.INSTANCE;
     }
 
     private static final class DatabaseHolder {
-        public static final FirebaseDatabase INSTANCE = FirebaseDatabase.getInstance();
+        public static final DatabaseReference INSTANCE;
+        private static final String DATABASE_VERSION = "v" + BuildConfig.VERSION_NAME.split("\\.")[0];
 
         static {
-            INSTANCE.setPersistenceEnabled(true);
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            database.setPersistenceEnabled(true);
+            INSTANCE = database.getReference()
+                    .child(BuildConfig.DEBUG ? "debug" : DATABASE_VERSION);
         }
     }
 }
