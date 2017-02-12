340287241c834327028bcddd958f192addba2efa
Alex Saveau <saveau.alexandre@gmail.com>
2017-02-12 12:31:37 -0800

    Fix Firebase mapping file upload using wrong `versionCode`
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
-  ./gradlew clean build publishApkProdRelease firebaseUploadProdReleaseProguardMapping
+  ./gradlew clean build publishApkProdRelease
 else
   ./gradlew clean assembleDebug check
 fi
--- upload.sh
+++ upload.sh
@@ -11,14 +11,18 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   mv Robot-Scouter/app/build/outputs/mapping/prod/release/mapping.txt uploads/mapping.txt
   cd uploads
 
-  APK_INFO=$(/usr/local/android-sdk/build-tools/25.0.2/aapt dump badging app-release.apk) &> /dev/null
-  VERSION_CODE=$(echo ${APK_INFO} | grep 'versionCode=' | awk -F: 'match($0,"versionCode="){ print substr($2,37,19)}' | tr -d "'") &> /dev/null
+  APK_DUMP=$(/usr/local/android-sdk/build-tools/25.0.2/aapt dump badging app-release.apk) &> /dev/null
+  VERSION_CODE="$(echo ${APK_DUMP} | grep -o -P "(?<=versionCode=\047).*(?=\047 versionName)")"
   git add mapping.txt app-release.apk
-  git commit -a -m "$(printf "${VERSION_CODE}\n Full apk dump: ${APK_INFO}")"
+  git commit -a -m "$(printf "${VERSION_CODE}\nFull apk dump:\n${APK_DUMP}")"
   git push -u origin master &> /dev/null
 
   cd ..
   cd Robot-Scouter
+
+  # TODO remove once Firebase fixes their plugin
+  sed -i "s/\(FirebaseCrashVersionCode=\).*\$/\1${VERSION_CODE}/" gradle.properties
+  ./gradlew firebaseUploadArchivedProguardMapping
 
   wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-141.0.0-linux-x86_64.tar.gz
   tar xf google-cloud-sdk-141.0.0-linux-x86_64.tar.gz
