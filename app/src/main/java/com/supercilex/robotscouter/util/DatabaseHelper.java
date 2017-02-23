91796110f642628ef6692fe8db42bd0a8c3261ab
Alex Saveau <saveau.alexandre@gmail.com>
2017-02-22 22:00:31 -0800

    Remove db versioning
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/util/DatabaseHelper.java
+++ app/src/main/java/com/supercilex/robotscouter/util/DatabaseHelper.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.supercilex.robotscouter.BuildConfig;
 
 public final class DatabaseHelper {
     private static final String QUERY_KEY = "query_key";
@@ -33,12 +32,10 @@ public final class DatabaseHelper {
     private static final class DatabaseHolder {
         public static final FirebaseDatabase DATABASE = FirebaseDatabase.getInstance();
         public static final DatabaseReference INSTANCE;
-        private static final String DATABASE_VERSION = "v" + BuildConfig.VERSION_NAME.split("\\.")[0];
 
         static {
             DATABASE.setPersistenceEnabled(true);
-            INSTANCE = DATABASE.getReference()
-                    .child(BuildConfig.DEBUG ? "debug" : DATABASE_VERSION);
+            INSTANCE = DATABASE.getReference();
         }
     }
 }
