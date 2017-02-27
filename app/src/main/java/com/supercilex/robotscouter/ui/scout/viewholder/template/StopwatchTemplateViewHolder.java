c1c0a0fb2c2d2a20e1fa9fdf1f9a6db0e589654e
Alex Saveau <saveau.alexandre@gmail.com>
2017-02-26 21:30:10 -0800

    Fix bug where stopwatch template name text would overlap start button
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/ui/scout/viewholder/template/StopwatchTemplateViewHolder.java
+++ app/src/main/java/com/supercilex/robotscouter/ui/scout/viewholder/template/StopwatchTemplateViewHolder.java
@@ -1,7 +1,10 @@
 package com.supercilex.robotscouter.ui.scout.viewholder.template;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.view.View;
 
+import com.supercilex.robotscouter.R;
 import com.supercilex.robotscouter.ui.scout.viewholder.StopwatchViewHolder;
 
 public class StopwatchTemplateViewHolder extends StopwatchViewHolder implements ScoutTemplateViewHolder {
@@ -13,6 +16,12 @@ public class StopwatchTemplateViewHolder extends StopwatchViewHolder implements 
     protected void bind() {
         super.bind();
         mName.setOnFocusChangeListener(this);
+
+        ConstraintLayout layout = (ConstraintLayout) itemView;
+        ConstraintSet set = new ConstraintSet();
+        set.clone(layout);
+        set.connect(R.id.list, ConstraintSet.TOP, R.id.name, ConstraintSet.BOTTOM, 0);
+        set.applyTo(layout);
     }
 
     @Override
