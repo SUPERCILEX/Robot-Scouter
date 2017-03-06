9e121d8d0b987441380bfce38f5a456a0da1ce48
Alex Saveau <saveau.alexandre@gmail.com>
2017-03-06 09:56:13 -0800

    Cleanup
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 if [ $TRAVIS_PULL_REQUEST = "false" ]; then
-  openssl aes-256-cbc -K $encrypted_b2fd50a80bbe_key -iv $encrypted_b2fd50a80bbe_iv -in secrets.tar.enc -out secrets.tar -d
+  openssl aes-256-cbc -K $encrypted_c4fd8e842577_key -iv $encrypted_c4fd8e842577_iv -in secrets.tar.enc -out secrets.tar -d &> /dev/null
   tar xvf secrets.tar
 else
   mv travis-dummies/google-services.json app/google-services.json
   mv travis-dummies/keystore.properties keystore.properties
   mv travis-dummies/keystore.jks app/keystore.jks
 fi
-
-set +xe
--- upload.sh
+++ upload.sh
@@ -3,13 +3,13 @@
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   cp app/build/outputs/apk/app-prod-release.apk app-release.apk
   cd ..
-  git clone --branch=master "https://SUPERCILEX:${git_mapping_login}@github.com/SUPERCILEX/mapping.txt-files.git" uploads &> /dev/null
+  git clone --branch=master "https://SUPERCILEX:${git_mapping_login}@github.com/SUPERCILEX/app-version-history.git" uploads &> /dev/null
   git config --global user.email "saveau.alexandre@gmail.com"
   git config --global user.name "Alex Saveau"
 
-  cp Robot-Scouter/app-release.apk uploads/app-release.apk
-  cp Robot-Scouter/app/build/outputs/mapping/prod/release/mapping.txt uploads/mapping.txt
-  cd uploads
+  cp Robot-Scouter/app-release.apk uploads/Robot-Scouter/app-release.apk
+  cp Robot-Scouter/app/build/outputs/mapping/prod/release/mapping.txt uploads/Robot-Scouter/mapping.txt
+  cd uploads/Robot-Scouter
 
   APK_DUMP=$(/usr/local/android-sdk/build-tools/25.0.2/aapt dump badging app-release.apk) &> /dev/null
   VERSION_CODE="$(echo ${APK_DUMP} | grep -o -P "(?<=versionCode=\047).*(?=\047 versionName)")"
@@ -18,15 +18,15 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   git commit -a -m "$(printf "${VERSION_CODE}\n${DIFF}\nFull apk dump:\n${APK_DUMP}")"
   git push -u origin master &> /dev/null
 
-  cd ..
+  cd ../..
   cd Robot-Scouter
 
   # TODO remove once Firebase fixes their plugin
   sed -i "s/\(FirebaseCrashVersionCode=\).*\$/\1${VERSION_CODE}/" gradle.properties
   ./gradlew firebaseUploadArchivedProguardMapping
 
-  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-141.0.0-linux-x86_64.tar.gz
-  tar xf google-cloud-sdk-141.0.0-linux-x86_64.tar.gz
+  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-146.0.0-linux-x86_64.tar.gz
+  tar xf google-cloud-sdk-146.0.0-linux-x86_64.tar.gz
   echo "y" | ./google-cloud-sdk/bin/gcloud components update alpha
   ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file app/google-play-auto-publisher.json
   ./google-cloud-sdk/bin/gcloud alpha test android run --async --app app-release.apk --device-ids m0,Nexus6P --os-version-ids 18,25 --orientations portrait --max-depth 100 --robo-directives team_number=2521 --project robot-scouter-app
