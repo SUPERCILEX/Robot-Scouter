c21a941ed9d86358f3a7ab5d043ad71f418bf6ce
Alex Saveau <SUPERCILEX@users.noreply.github.com>
2017-03-31 13:03:34 -0700

    Auto deploy security rules and default template so people can contribute to those too (#93)

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 if [ $TRAVIS_PULL_REQUEST = "false" ]; then
   openssl aes-256-cbc -K $encrypted_8e2b28f9b71e_key -iv $encrypted_8e2b28f9b71e_iv -in secrets.tar.enc -out secrets.tar -d &> /dev/null
   tar xvf secrets.tar
+
+  if [ $TRAVIS_BRANCH = 'master' ]; then
+    npm install -g firebase-tools
+  fi
 else
   mv travis-dummies/google-services.json app/google-services.json
   mv travis-dummies/google-play-auto-publisher.json app/google-play-auto-publisher.json
