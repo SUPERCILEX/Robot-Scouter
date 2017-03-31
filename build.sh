2f09f6f19de30c324010af8335c841a778055063
Alex Saveau <SUPERCILEX@users.noreply.github.com>
2017-03-31 13:03:34 -0700

    Auto deploy security rules and default template so people can contribute to those too (#93)

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
   ./gradlew clean build publishApkProdRelease
+
+  cd firebase
+  firebase deploy
+  firebase database:set --confirm /default-template default-template.json
+  cd ..
 else
   ./gradlew clean assembleDebug check
 fi
