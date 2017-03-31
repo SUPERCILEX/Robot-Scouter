24af553fb022b709c2d8f9e38f3718fa92156dc4
Alex Saveau <saveau.alexandre@gmail.com>
2017-03-31 12:51:27 -0700

    Prep for merge
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -xe
 
-cd firebase
-firebase deploy
-firebase database:set --confirm /default-template default-template.json
-cd ..
-
-
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
   ./gradlew clean build publishApkProdRelease
+
+  cd firebase
+  firebase deploy
+  firebase database:set --confirm /default-template default-template.json
+  cd ..
 else
   ./gradlew clean assembleDebug check
 fi
