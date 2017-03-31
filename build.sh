1c179da74b3f979f3bbf53da415753462458d792
Alex Saveau <saveau.alexandre@gmail.com>
2017-03-31 12:39:55 -0700

    Install firebase-tools
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -xe
 
+cd firebase
+firebase deploy
+firebase database:set --confirm /default-template default-template.json
+cd ..
+
+
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
   ./gradlew clean build publishApkProdRelease
 else
   ./gradlew clean assembleDebug check
 fi
-
-  cd firebase
-  firebase deploy
-  firebase database:set --confirm /default-template default-template.json
-  cd ..
 
 set +xe
