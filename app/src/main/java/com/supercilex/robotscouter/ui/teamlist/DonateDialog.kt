a725c8d29e1e81c5298e18be6395b11fa944b26e
Alex Saveau <saveau.alexandre@gmail.com>
2017-07-02 22:42:59 -0700

    Add ProgressBar and handle special case:
    where we weren't able to consume purchase properly
    
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/ui/teamlist/DonateDialog.kt
+++ app/src/main/java/com/supercilex/robotscouter/ui/teamlist/DonateDialog.kt
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v7.app.AlertDialog
 import android.view.View
 import android.widget.CheckBox
+import android.widget.ProgressBar
 import android.widget.SeekBar
 import android.widget.TextView
 import com.android.billingclient.api.BillingClient
@@ -16,6 +17,7 @@ import com.android.billingclient.api.BillingClientStateListener
 import com.android.billingclient.api.BillingFlowParams
 import com.android.billingclient.api.Purchase
 import com.android.billingclient.api.PurchasesUpdatedListener
+import com.google.android.gms.tasks.Continuation
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.TaskCompletionSource
 import com.google.android.gms.tasks.Tasks
@@ -28,6 +30,8 @@ import com.supercilex.robotscouter.util.getUid
 
 class DonateDialog : ManualDismissDialog(), SeekBar.OnSeekBarChangeListener, PurchasesUpdatedListener, BillingClientStateListener {
     private val root by lazy { View.inflate(context, R.layout.dialog_donate, null) }
+    private val content by lazy { root.findViewById<View>(R.id.content) }
+    private val progress by lazy { root.findViewById<ProgressBar>(R.id.progress) }
     private val amountTextView by lazy { root.findViewById<TextView>(R.id.amount_textview) }
     private val amountSeekBar by lazy { root.findViewById<SeekBar>(R.id.amount) }
     private val monthlyCheckBox by lazy { root.findViewById<CheckBox>(R.id.monthly) }
@@ -69,15 +73,36 @@ class DonateDialog : ManualDismissDialog(), SeekBar.OnSeekBarChangeListener, Pur
     }
 
     override fun onAttemptDismiss(): Boolean {
+        updateProgress(true)
+
         billingClientReadyTask.task.addOnSuccessListener(activity) {
+            val sku = "${amountSeekBar.progress + 1}$SKU_BASE${if (monthlyCheckBox.isChecked) "monthly" else "single"}"
+            val type = if (monthlyCheckBox.isChecked) BillingClient.SkuType.SUBS else BillingClient.SkuType.INAPP
+
             val result = billingClient.launchBillingFlow(activity, BillingFlowParams.Builder()
-                    .setSku("${amountSeekBar.progress + 1}$SKU_BASE${if (monthlyCheckBox.isChecked) "monthly" else "single"}")
-                    .setType(if (monthlyCheckBox.isChecked) BillingClient.SkuType.SUBS else BillingClient.SkuType.INAPP)
+                    .setSku(sku)
+                    .setType(type)
                     .setAccountId(getUid()?.hashCode().toString())
                     .build())
 
-            if (result != BillingClient.BillingResponse.OK) {
-                FirebaseCrash.report(IllegalStateException("Unknonwn puchase error: $result"))
+            if (result != BillingResponse.OK) {
+                if (result == BillingResponse.ITEM_ALREADY_OWNED) {
+                    billingClient.queryPurchaseHistoryAsync(type) {
+                        if (it.responseCode != BillingResponse.OK) {
+                            FirebaseCrash.report(
+                                    IllegalStateException("Purchase fetch failed with code ${it.responseCode}"))
+                            showError()
+                            return@queryPurchaseHistoryAsync
+                        }
+
+                        getConsumePurchasesTask(it.purchasesList)
+                                .addOnSuccessListener { onAttemptDismiss() }
+                                .addOnFailureListener { showError() }
+                    }
+                    return@addOnSuccessListener
+                }
+
+                FirebaseCrash.report(IllegalStateException("Unknown purchase error: $result"))
                 showError()
             }
         }
@@ -87,33 +112,54 @@ class DonateDialog : ManualDismissDialog(), SeekBar.OnSeekBarChangeListener, Pur
 
     override fun onPurchasesUpdated(responseCode: Int, purchases: List<Purchase>?) {
         if (responseCode == BillingResponse.OK && purchases != null) {
-            val consumptions: MutableList<Task<Pair<String, Int>>> = ArrayList()
-
-            for (purchase in purchases) {
-                val consumption = TaskCompletionSource<Pair<String, Int>>()
-                billingClient.consumeAsync(purchase.purchaseToken) { purchaseToken, resultCode ->
-                    consumption.setResult(purchaseToken to resultCode)
-                }
-                consumptions.add(consumption.task)
-            }
-
-            Tasks.whenAll(consumptions).addOnSuccessListener {
-                if (consumptions.find { !it.isSuccessful } == null) {
-                    dismiss()
-                } else {
-                    showError()
-                }
-            }
+            getConsumePurchasesTask(purchases)
+                    .addOnSuccessListener { dismiss() }
+                    .addOnFailureListener { showError() }
         } else if (responseCode == BillingResponse.USER_CANCELED) {
             Snackbar.make(root, R.string.donate_cancel_message, Snackbar.LENGTH_LONG).show()
         } else {
-            FirebaseCrash.report(IllegalStateException("Unknonwn puchase error: $responseCode"))
+            FirebaseCrash.report(IllegalStateException("Unknown purchase error: $responseCode"))
             showError()
         }
     }
 
-    private fun showError() =
-            Snackbar.make(root, R.string.general_error, Snackbar.LENGTH_LONG).show()
+    private fun getConsumePurchasesTask(purchases: List<Purchase>): Task<Nothing> {
+        val consumptions: MutableList<Task<String>> = ArrayList()
+
+        for (purchase in purchases) {
+            val consumption = TaskCompletionSource<String>()
+            billingClient.consumeAsync(purchase.purchaseToken) { purchaseToken, resultCode ->
+                if (resultCode == BillingResponse.OK || resultCode == BillingResponse.ITEM_NOT_OWNED) {
+                    consumption.setResult(purchaseToken)
+                } else {
+                    val ex = IllegalStateException("Consumption failed with code $resultCode")
+                    FirebaseCrash.report(ex)
+                    consumption.setException(ex)
+                }
+            }
+            consumptions.add(consumption.task)
+        }
+
+        return Tasks.whenAll(consumptions).continueWithTask(Continuation<Void, Task<Nothing>> {
+            val unsuccessfulConsumptions = consumptions.find { !it.isSuccessful }
+
+            return@Continuation if (unsuccessfulConsumptions == null) {
+                Tasks.forResult(null)
+            } else {
+                Tasks.forException(unsuccessfulConsumptions.exception!!)
+            }
+        })
+    }
+
+    private fun updateProgress(isDoingAsyncWork: Boolean) {
+        content.visibility = if (isDoingAsyncWork) View.GONE else View.VISIBLE
+        progress.visibility = if (isDoingAsyncWork) View.VISIBLE else View.GONE
+    }
+
+    private fun showError() {
+        updateProgress(false)
+        Snackbar.make(root, R.string.general_error, Snackbar.LENGTH_LONG).show()
+    }
 
     override fun onBillingSetupFinished(resultCode: Int) = billingClientReadyTask.setResult(resultCode)
 
