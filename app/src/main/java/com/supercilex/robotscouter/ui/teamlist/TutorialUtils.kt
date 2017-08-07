7c4f01f1a367937439e228443183c6d05073a630
Alex Saveau <saveau.alexandre@gmail.com>
2017-08-07 11:57:55 -0700

    Update MaterialTapTargetPrompt
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/ui/teamlist/TutorialUtils.kt
+++ app/src/main/java/com/supercilex/robotscouter/ui/teamlist/TutorialUtils.kt
@@ -1,7 +1,6 @@
 package com.supercilex.robotscouter.ui.teamlist
 
 import android.app.Activity
-import android.view.MotionEvent
 import com.supercilex.robotscouter.R
 import com.supercilex.robotscouter.util.data.hasShownAddTeamTutorial
 import com.supercilex.robotscouter.util.data.hasShownSignInTutorial
@@ -9,24 +8,19 @@ import com.supercilex.robotscouter.util.data.setHasShownAddTeamTutorial
 import com.supercilex.robotscouter.util.data.setHasShownSignInTutorial
 import uk.co.samuelwall.materialtaptargetprompt.MaterialTapTargetPrompt
 
-private object EmptyOnHidePromptListener : MaterialTapTargetPrompt.OnHidePromptListener {
-    override fun onHidePrompt(event: MotionEvent, tappedTarget: Boolean) {}
-
-    override fun onHidePromptComplete() {}
-}
-
 fun showCreateFirstTeamPrompt(activity: Activity): MaterialTapTargetPrompt? {
     val appContext = activity.applicationContext
     if (!hasShownAddTeamTutorial(appContext)) {
         return MaterialTapTargetPrompt.Builder(activity, R.style.RobotScouter_Tutorial)
                 .setTarget(R.id.fab)
                 .setPrimaryText(R.string.create_first_team)
+                // TODO Fix https://github.com/sjwall/MaterialTapTargetPrompt/pull/81 before official release
                 .setAutoDismiss(false)
-                .setOnHidePromptListener(object : MaterialTapTargetPrompt.OnHidePromptListener by EmptyOnHidePromptListener {
-                    override fun onHidePrompt(event: MotionEvent, tappedTarget: Boolean) {
-                        setHasShownAddTeamTutorial(appContext, tappedTarget)
+                .setPromptStateChangeListener { _, state ->
+                    if (state == MaterialTapTargetPrompt.STATE_FOCAL_PRESSED) {
+                        setHasShownAddTeamTutorial(appContext, true)
                     }
-                })
+                }
                 .show()
     }
     return null
@@ -39,11 +33,11 @@ fun showSignInPrompt(activity: Activity) {
                 .setTarget(R.id.action_sign_in)
                 .setPrimaryText(R.string.sign_in)
                 .setSecondaryText(R.string.sign_in_rationale)
-                .setOnHidePromptListener(object : MaterialTapTargetPrompt.OnHidePromptListener by EmptyOnHidePromptListener {
-                    override fun onHidePrompt(event: MotionEvent, tappedTarget: Boolean) {
-                        setHasShownSignInTutorial(appContext, tappedTarget)
+                .setPromptStateChangeListener { _, state ->
+                    if (state == MaterialTapTargetPrompt.STATE_FOCAL_PRESSED) {
+                        setHasShownSignInTutorial(appContext, true)
                     }
-                })
+                }
                 .show()
     }
 }
