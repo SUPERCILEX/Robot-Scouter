SUPERCILEX/Robot-Scouter

commit 17cf24826b69e774584c6932cdafcccf6ec9876c

Add support for long-press when adding scout (#82)
Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

app/src/main/java/com/supercilex/robotscouter/ui/scouting/scoutlist/ScoutListFragmentBase.kt
@@ -22,6 +22,7 @@ import com.supercilex.robotscouter.ui.TeamDetailsDialog
 import com.supercilex.robotscouter.ui.TeamSharer
 import com.supercilex.robotscouter.ui.scouting.templatelist.TemplateListActivity
 import com.supercilex.robotscouter.util.data.KEY_ADD_SCOUT
+import com.supercilex.robotscouter.util.data.KEY_OVERRIDE_TEMPLATE_KEY
 import com.supercilex.robotscouter.util.data.getScoutBundle
 import com.supercilex.robotscouter.util.data.getTabKey
 import com.supercilex.robotscouter.util.data.model.TeamHolder
@@ -139,6 +140,10 @@ abstract class ScoutListFragmentBase : FragmentBase(),
         return true
     }
 
+    fun onAddScout(overrideTemplateKey: String) {
+        pagerAdapter!!.currentTabKey = team.addScout(overrideTemplateKey)
+    }
+
     private fun initScoutList() {
         val tabLayout = rootView.findViewById<TabLayout>(R.id.tabs)
         val viewPager = rootView.findViewById<ViewPager>(R.id.viewpager)
@@ -150,7 +155,8 @@ abstract class ScoutListFragmentBase : FragmentBase(),
 
         if (arguments.getBoolean(KEY_ADD_SCOUT, false)) {
             arguments.remove(KEY_ADD_SCOUT)
-            pagerAdapter!!.currentTabKey = team.addScout()
+            pagerAdapter!!.currentTabKey =
+                    team.addScout(arguments.getString(KEY_OVERRIDE_TEMPLATE_KEY, null))
         }
     }
 

app/src/main/java/com/supercilex/robotscouter/ui/scouting/scoutlist/AppBarViewHolderBase.kt
@@ -34,6 +34,7 @@ import com.supercilex.robotscouter.util.data.model.copyMediaInfo
 import com.supercilex.robotscouter.util.data.model.forceUpdateTeam
 import com.supercilex.robotscouter.util.data.model.isOutdatedMedia
 import com.supercilex.robotscouter.util.ui.TeamMediaCreator
+import com.supercilex.robotscouter.util.ui.TemplateSelectorDialog
 
 open class AppBarViewHolderBase(private val fragment: LifecycleFragment,
                                 rootView: View,
@@ -134,6 +135,9 @@ open class AppBarViewHolderBase(private val fragment: LifecycleFragment,
                 fragment.getString(R.string.visit_team_website_on_tba, team.number)
         visitTeamWebsiteItem.title = fragment.getString(R.string.visit_team_website, team.number)
         if (!onScoutingReadyTask.isComplete) newScoutItem.isVisible = false
+        toolbar.post {
+            fragment.view!!.findViewById<View>(R.id.action_new_scout).setOnLongClickListener(this)
+        }
 
         onMenuReadyTask.trySetResult(null)
         bindMenu()
@@ -156,10 +160,14 @@ open class AppBarViewHolderBase(private val fragment: LifecycleFragment,
             Color.green(opaque),
             Color.blue(opaque))
 
-    override fun onLongClick(v: View): Boolean = if (v.id == R.id.backdrop) {
-        ShouldUploadMediaToTbaDialog.show(fragment)
-        true
-    } else false
+    override fun onLongClick(v: View): Boolean {
+        when {
+            v.id == R.id.backdrop -> ShouldUploadMediaToTbaDialog.show(fragment)
+            v.id == R.id.action_new_scout -> TemplateSelectorDialog.show(fragment.childFragmentManager)
+            else -> return false
+        }
+        return true
+    }
 
     override fun onStartCapture(shouldUploadMediaToTba: Boolean) =
             mediaCapture.startCapture(shouldUploadMediaToTba)

app/src/main/java/com/supercilex/robotscouter/ui/scouting/scoutlist/viewholder/StopwatchViewHolder.java
@@ -215,7 +215,7 @@ public class StopwatchViewHolder extends MetricViewHolderBase<Metric<List<Long>>
                     stopwatch.getContext(), R.color.colorAccent) : Color.WHITE);
             stopwatch.setActivated(mIsRunning);
             stopwatch.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                    mIsRunning ? R.drawable.ic_timer_off_color_accent_24dp : R.drawable.ic_timer_white_24dp,
+                    mIsRunning ? R.drawable.ic_timer_off_accent_24dp : R.drawable.ic_timer_white_24dp,
                     0,
                     0,
                     0);

app/src/main/java/com/supercilex/robotscouter/ui/scouting/templatelist/SpinnerItemsHolder.kt
@@ -2,7 +2,6 @@ package com.supercilex.robotscouter.ui.scouting.templatelist
 
 import android.app.Application
 import android.os.Bundle
-import com.firebase.ui.database.ClassSnapshotParser
 import com.firebase.ui.database.FirebaseArray
 import com.firebase.ui.database.ObservableSnapshotArray
 import com.google.firebase.database.DatabaseReference
@@ -24,7 +23,7 @@ class SpinnerItemsHolder(app: Application) : ViewModelBase<Bundle>(app) {
     override fun onCreate(args: Bundle) {
         ref = getRef(args)
         selectedValueKey = args.getString(FIREBASE_SELECTED_VALUE_KEY)
-        spinnerItems = FirebaseArray(ref, ClassSnapshotParser(String::class.java))
+        spinnerItems = FirebaseArray(ref, String::class.java)
         spinnerItems.addChangeEventListener(listener)
     }
 

app/src/main/java/com/supercilex/robotscouter/ui/scouting/templatelist/SpinnerTemplateDialog.kt
@@ -42,6 +42,8 @@ class SpinnerTemplateDialog : LifecycleDialogFragment(), View.OnClickListener {
                 R.layout.scout_template_spinner_item,
                 SpinnerItemViewHolder::class.java,
                 this) {
+            private val cardListHelper by lazy { CardListHelper(this, recyclerView) }
+
             override fun getItem(position: Int) = itemTouchCallback.getItem(position)
 
             override fun getRef(position: Int) = itemTouchCallback.getRef(position)
@@ -85,7 +87,6 @@ class SpinnerTemplateDialog : LifecycleDialogFragment(), View.OnClickListener {
             }
         }
     }
-    private val cardListHelper: CardListHelper by lazy { CardListHelper(adapter, recyclerView) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

app/src/main/java/com/supercilex/robotscouter/ui/scouting/templatelist/TemplateListFragment.kt
@@ -127,4 +127,3 @@ class TemplateListFragment : FragmentBase(), View.OnClickListener, OnBackPressed
                 TemplateListFragment().apply { arguments = getTabKeyBundle(templateKey) }
     }
 }
-

app/src/main/java/com/supercilex/robotscouter/ui/teamlist/TeamViewHolder.kt
@@ -23,6 +23,7 @@ import com.supercilex.robotscouter.R
 import com.supercilex.robotscouter.data.model.Team
 import com.supercilex.robotscouter.ui.TeamDetailsDialog
 import com.supercilex.robotscouter.util.data.getScoutBundle
+import com.supercilex.robotscouter.util.ui.TemplateSelectorDialog
 import com.supercilex.robotscouter.util.ui.animateCircularReveal
 
 class TeamViewHolder @Keep constructor(itemView: View) :
@@ -94,6 +95,7 @@ class TeamViewHolder @Keep constructor(itemView: View) :
         mediaImageView.setOnClickListener(this)
         mediaImageView.setOnLongClickListener(this)
         newScoutButton.setOnClickListener(this)
+        newScoutButton.setOnLongClickListener(this)
         itemView.setOnClickListener(this)
         itemView.setOnLongClickListener(this)
     }
@@ -134,15 +136,14 @@ class TeamViewHolder @Keep constructor(itemView: View) :
     }
 
     override fun onLongClick(v: View): Boolean {
-        if (isItemSelected || couldItemBeSelected || v.id == R.id.root) {
-            onTeamContextMenuRequested()
-            return true
-        } else if (v.id == R.id.media) {
-            TeamDetailsDialog.show(fragment.childFragmentManager, team)
-            return true
+        when {
+            isItemSelected || couldItemBeSelected || v.id == R.id.root -> onTeamContextMenuRequested()
+            v.id == R.id.media -> TeamDetailsDialog.show(fragment.childFragmentManager, team)
+            v.id == R.id.new_scout -> TemplateSelectorDialog.show(
+                    fragment.childFragmentManager, team)
+            else -> return false
         }
-
-        return false
+        return true
     }
 
     private fun onTeamContextMenuRequested() {

app/src/main/java/com/supercilex/robotscouter/util/data/ArgUtils.kt
@@ -11,6 +11,7 @@ import com.supercilex.robotscouter.util.data.model.toBundle
 const val TAB_KEY = "tab_key"
 const val SCOUT_ARGS_KEY = "scout_args"
 const val KEY_ADD_SCOUT = "add_scout"
+const val KEY_OVERRIDE_TEMPLATE_KEY = "override_template_key"
 
 fun Parcel.readBooleanCompat() = getBooleanForInt(readInt())
 
@@ -31,8 +32,17 @@ fun getTabKeyBundle(key: String?) = Bundle().apply { putString(TAB_KEY, key) }
 
 fun getTabKey(bundle: Bundle): String? = bundle.getString(TAB_KEY)
 
-fun getScoutBundle(team: Team, addScout: Boolean = false, scoutKey: String? = null) =
-        team.toBundle().apply {
-            putBoolean(KEY_ADD_SCOUT, addScout)
-            putAll(getTabKeyBundle(scoutKey))
-        }
+fun getScoutBundle(team: Team,
+                   addScout: Boolean = false,
+                   overrideTemplateKey: String? = null,
+                   scoutKey: String? = null): Bundle {
+    if (!addScout && overrideTemplateKey != null) {
+        throw IllegalArgumentException("Can't use an override key without adding a scout.")
+    }
+
+    return team.toBundle().apply {
+        putBoolean(KEY_ADD_SCOUT, addScout)
+        putString(KEY_OVERRIDE_TEMPLATE_KEY, overrideTemplateKey)
+        putAll(getTabKeyBundle(scoutKey))
+    }
+}

commit da6f390aa896a127e043857b37ecd3dac4e318dd

Fix regression introduced in 17cf24826b69e774584c6932cdafcccf6ec9876c
Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

app/src/main/java/com/supercilex/robotscouter/ui/scouting/scoutlist/ScoutListFragmentBase.kt
@@ -56,7 +56,7 @@ abstract class ScoutListFragmentBase : FragmentBase(),
         return scoutKey
     }
     protected val bundle: Bundle
-        get() = getScoutBundle(team, arguments.getBoolean(KEY_ADD_SCOUT), scoutKey)
+        get() = getScoutBundle(team, arguments.getBoolean(KEY_ADD_SCOUT), scoutKey = scoutKey)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)