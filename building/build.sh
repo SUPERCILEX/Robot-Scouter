0ca50afc7ceb4cffd752b7406ae42596605ebec5
Alex Saveau <saveau.alexandre@gmail.com>
2017-10-15 19:37:14 -0700

    Fix deploy bugs and cleanup
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- building/build.sh
+++ building/build.sh
@@ -3,9 +3,10 @@
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
   ./gradlew clean build publishApkRelease
 
-  cd firebase
-  firebase deploy
-  cd ..
+# TODO It's broken in the latest release
+#  cd firebase
+#  firebase deploy
+#  cd ..
 else
   ./gradlew clean assembleDebug check
 fi
