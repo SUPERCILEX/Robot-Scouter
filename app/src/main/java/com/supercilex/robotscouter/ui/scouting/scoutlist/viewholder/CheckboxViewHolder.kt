943283286425d3dc711e0ca32720df9b51c0027a
Alex Saveau <saveau.alexandre@gmail.com>
2017-11-02 23:30:22 -0700

    Move checkbox to the right of text for consistent UI
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/ui/scouting/scoutlist/viewholder/CheckboxViewHolder.kt
+++ app/src/main/java/com/supercilex/robotscouter/ui/scouting/scoutlist/viewholder/CheckboxViewHolder.kt
@@ -1,21 +1,28 @@
 package com.supercilex.robotscouter.ui.scouting.scoutlist.viewholder
 
-import android.support.annotation.CallSuper
 import android.view.View
 import android.widget.CheckBox
+import android.widget.TextView
+import com.supercilex.robotscouter.R
 import com.supercilex.robotscouter.data.model.Metric
 import com.supercilex.robotscouter.ui.scouting.MetricViewHolderBase
+import kotterknife.bindView
 
 open class CheckboxViewHolder(
         itemView: View
-) : MetricViewHolderBase<Metric.Boolean, Boolean, CheckBox>(itemView),
+) : MetricViewHolderBase<Metric.Boolean, Boolean, TextView>(itemView),
         View.OnClickListener {
+    protected val checkbox: CheckBox by bindView(R.id.checkbox)
+
     public override fun bind() {
         super.bind()
-        name.isChecked = metric.value
+        checkbox.isChecked = metric.value
+        checkbox.setOnClickListener(this)
         name.setOnClickListener(this)
     }
 
-    @CallSuper
-    override fun onClick(v: View) = updateMetricValue(name.isChecked)
+    override fun onClick(v: View) {
+        if (v.id == R.id.checkbox) updateMetricValue(checkbox.isChecked)
+        if (v.id == R.id.name) checkbox.performClick()
+    }
 }
--- app/src/main/java/com/supercilex/robotscouter/ui/scouting/templatelist/viewholder/CheckboxTemplateViewHolder.kt
+++ app/src/main/java/com/supercilex/robotscouter/ui/scouting/templatelist/viewholder/CheckboxTemplateViewHolder.kt
@@ -4,25 +4,26 @@ import android.view.View
 import android.widget.EditText
 import com.supercilex.robotscouter.R
 import com.supercilex.robotscouter.ui.scouting.scoutlist.viewholder.CheckboxViewHolder
+import com.supercilex.robotscouter.util.unsafeLazy
 import kotterknife.bindView
 
 class CheckboxTemplateViewHolder(itemView: View) : CheckboxViewHolder(itemView), TemplateViewHolder {
     override val reorder: View by bindView(R.id.reorder)
-    override val nameEditor: EditText by bindView(R.id.checkbox_name)
+    override val nameEditor: EditText by unsafeLazy { name as EditText }
 
     override fun bind() {
         super.bind()
-        name.text = null
-        nameEditor.setText(metric.name)
-        nameEditor.onFocusChangeListener = this
+        name.setOnClickListener(null)
+        name.text = metric.name
+        name.onFocusChangeListener = this
     }
 
     override fun onClick(v: View) {
-        if (nameEditor.hasFocus()) updateMetricName(nameEditor.text.toString())
-        super.onClick(v)
+        if (name.hasFocus()) updateMetricName(name.text.toString())
+        if (v.id == R.id.checkbox) updateMetricValue(checkbox.isChecked)
     }
 
     override fun onFocusChange(v: View, hasFocus: Boolean) {
-        if (!hasFocus) updateMetricName(nameEditor.text.toString())
+        if (!hasFocus) updateMetricName(name.text.toString())
     }
 }
