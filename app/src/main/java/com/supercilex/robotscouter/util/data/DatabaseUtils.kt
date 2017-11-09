SUPERCILEX/Robot-Scouter

commit c3a3db3f81a001eb2e95ca4b8fd3298adb93cc31

Support null observable snapshot transformations to fix crashes
Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

app/src/main/java/com/supercilex/robotscouter/util/data/DatabaseUtils.kt
@@ -145,7 +145,15 @@ inline fun <T, R> LiveData<T>.observeOnce(
 
 fun <T> LiveData<ObservableSnapshotArray<T>>.observeOnDataChanged(
 ): LiveData<ObservableSnapshotArray<T>> = Transformations.switchMap(this) {
-    it.asLiveData()
+    it?.asLiveData()
+            // This "cast" is safe because the list is empty and we will never parse anything
+            ?: object : ObservableSnapshotArray<T>(@Suppress("UNCHECKED_CAST") { Unit as T }) {
+        init {
+            notifyOnDataChanged()
+        }
+
+        override fun getSnapshots() = emptyList<DocumentSnapshot>()
+    }.asLiveData()
 }
 
 fun <T> ObservableSnapshotArray<T>.asLiveData(): LiveData<ObservableSnapshotArray<T>> {

commit 76b903ea65fe64bdd29e16e03903749f5c12ece8

Quickfix crash introduced in c3a3db3f81a001eb2e95ca4b8fd3298adb93cc31
Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

app/src/main/java/com/supercilex/robotscouter/util/data/DatabaseUtils.kt
@@ -152,7 +152,7 @@ fun <T> LiveData<ObservableSnapshotArray<T>>.observeOnDataChanged(
             notifyOnDataChanged()
         }
 
-        override fun getSnapshots() = emptyList<DocumentSnapshot>()
+        override fun getSnapshots() = emptyList<DocumentSnapshot>().toMutableList()
     }.asLiveData()
 }
 