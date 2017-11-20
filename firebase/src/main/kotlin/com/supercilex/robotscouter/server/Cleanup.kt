ee6777e86d0fc1525d108baa6fcbfd0050b98637
Alex Saveau <saveau.alexandre@gmail.com>
2017-11-20 15:44:41 -0800

    Progress
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- firebase/src/main/kotlin/com/supercilex/robotscouter/server/Cleanup.kt
+++ firebase/src/main/kotlin/com/supercilex/robotscouter/server/Cleanup.kt
@@ -1,6 +1,11 @@
 package com.supercilex.robotscouter.server
 
 import com.supercilex.robotscouter.server.utils.FIRESTORE_EMAIL
+import com.supercilex.robotscouter.server.utils.FIRESTORE_METRICS
+import com.supercilex.robotscouter.server.utils.FIRESTORE_PHONE_NUMBER
+import com.supercilex.robotscouter.server.utils.FIRESTORE_SCOUTS
+import com.supercilex.robotscouter.server.utils.FIRESTORE_TIMESTAMP
+import com.supercilex.robotscouter.server.utils.delete
 import com.supercilex.robotscouter.server.utils.getTeamsQuery
 import com.supercilex.robotscouter.server.utils.getTemplatesQuery
 import com.supercilex.robotscouter.server.utils.getTrashedTeamsQuery
@@ -9,12 +14,14 @@ import com.supercilex.robotscouter.server.utils.toTeamString
 import com.supercilex.robotscouter.server.utils.toTemplateString
 import com.supercilex.robotscouter.server.utils.types.DocumentSnapshot
 import com.supercilex.robotscouter.server.utils.types.Query
+import com.supercilex.robotscouter.server.utils.userPrefs
 import com.supercilex.robotscouter.server.utils.users
 import kotlin.js.Promise
 
 private const val FIRESTORE_LAST_LOGIN = "lastLogin"
 private const val MAX_INACTIVE_USER_DAYS = 365
-private const val MAX_INACTIVE_ANONYMOUS_USER_DAYS = 30 // TODO
+private const val MAX_INACTIVE_ANONYMOUS_USER_DAYS = 60
+private const val TRASH_TIMEOUT = 30
 
 fun deleteUnusedData(): Promise<*> {
     console.log("Looking for users that haven't opened Robot Scouter for over a year" +
@@ -25,19 +32,26 @@ fun deleteUnusedData(): Promise<*> {
                     "<",
                     modules.moment().subtract(MAX_INACTIVE_USER_DAYS, "days").toDate()
             )),
+            deleteUnusedData(users.where( // TODO remove once enough people have upgraded
+                    FIRESTORE_LAST_LOGIN,
+                    "<",
+                    modules.moment().subtract(30, "days").toDate()
+            ).where(
+                    FIRESTORE_EMAIL, "==", null
+            )),
             deleteUnusedData(users.where(
                     FIRESTORE_LAST_LOGIN,
                     "<",
                     modules.moment().subtract(MAX_INACTIVE_ANONYMOUS_USER_DAYS, "days").toDate()
             ).where(
                     FIRESTORE_EMAIL, "==", null
-            )/*.where( // TODO
+            ).where(
                     FIRESTORE_PHONE_NUMBER, "==", null
-            )*/)
+            ))
     ))
 }
 
-private fun deleteUnusedData(query: Query): Promise<Unit> = query.get().then { users ->
+private fun deleteUnusedData(userQuery: Query): Promise<Unit> = userQuery.get().then { users ->
     Promise.all(users.docs.map {
         deleteAllData(it)
     }.toTypedArray())
@@ -50,7 +64,11 @@ fun emptyTrash(): Promise<*> {
         Promise.all(users.docs.map {
             val userId = it.id
             Promise.all(arrayOf(
-                    deleteTeams(getTrashedTeamsQuery(userId)),
+                    deleteTeams(getTrashedTeamsQuery(userId).where(
+                            FIRESTORE_TIMESTAMP,
+                            "<",
+                            modules.moment().subtract(TRASH_TIMEOUT, "days").toDate()
+                    )),
                     deleteTemplates(getTrashedTemplatesQuery(userId))
             ))
         }.toTypedArray())
@@ -70,9 +88,9 @@ private fun deleteAllData(user: DocumentSnapshot): Promise<Unit> {
 
 private fun deleteUser(user: DocumentSnapshot): Promise<Unit> {
     console.log("Deleting user: ${user.id}")
-//    user.userPrefs.delete()
-//    user.ref.delete()
-    return Promise.resolve(Unit)
+    return user.userPrefs.delete().then {
+        user.ref.delete()
+    }.then { Unit }
 }
 
 private fun deleteTeams(query: Query): Promise<Unit> = query.get().then { teams ->
@@ -83,12 +101,11 @@ private fun deleteTeams(query: Query): Promise<Unit> = query.get().then { teams 
 
 private fun deleteTeam(team: DocumentSnapshot): Promise<Unit> {
     console.log("Deleting team: ${team.toTeamString()}")
-//    team.ref.collection(FIRESTORE_SCOUTS).delete {
-//        it.ref.collection(FIRESTORE_METRICS).delete()
-//    }.then {
-//        team.ref.delete()
-//    }
-    return Promise.resolve(Unit)
+    return team.ref.collection(FIRESTORE_SCOUTS).delete {
+        it.ref.collection(FIRESTORE_METRICS).delete()
+    }.then {
+        team.ref.delete()
+    }.then { Unit }
 }
 
 private fun deleteTemplates(query: Query): Promise<Unit> = query
@@ -100,5 +117,7 @@ private fun deleteTemplates(query: Query): Promise<Unit> = query
 
 private fun deleteTemplate(template: DocumentSnapshot): Promise<Unit> {
     console.log("Deleting template: ${template.toTemplateString()}")
-    return Promise.resolve(Unit)
+    return template.ref.collection(FIRESTORE_METRICS).delete().then {
+        template.ref.delete()
+    }.then { Unit }
 }
