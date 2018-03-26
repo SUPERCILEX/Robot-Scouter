1a22faec1ace8521cf1c190a549c698a29e1e379
Alex Saveau <saveau.alexandre@gmail.com>
2018-03-26 01:20:23 -0700

    Add support for all popular media types
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/supercilex/robotscouter/data/remote/TbaDownloader.kt
+++ app/src/main/java/com/supercilex/robotscouter/data/remote/TbaDownloader.kt
@@ -7,6 +7,11 @@ import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.supercilex.robotscouter.RobotScouter
 import com.supercilex.robotscouter.data.model.Team
+import com.supercilex.robotscouter.data.remote.TbaDownloader.Media.ChiefDelphi
+import com.supercilex.robotscouter.data.remote.TbaDownloader.Media.Imgur
+import com.supercilex.robotscouter.data.remote.TbaDownloader.Media.Instagram
+import com.supercilex.robotscouter.data.remote.TbaDownloader.Media.Unsupported
+import com.supercilex.robotscouter.data.remote.TbaDownloader.Media.YouTube
 import org.jetbrains.anko.runOnUiThread
 import retrofit2.Response
 
@@ -52,29 +57,35 @@ class TbaDownloader private constructor(
 
         if (cannotContinue(response)) return
 
-        var media: String? = null
+        val media = response.body()!!.asSequence().map {
+            val mediaObject: JsonObject = it.asJsonObject
 
-        for (element: JsonElement in response.body()!!) {
-            val mediaObject: JsonObject = element.asJsonObject
-            val mediaType: String = mediaObject.get("type").asString
+            val type = mediaObject.get("type").asString
+            val key = mediaObject.get("foreign_key").asString
+            val preferred = mediaObject.get("preferred").asBoolean
 
-            if (mediaType == IMGUR) {
-                media = "https://i.imgur.com/${mediaObject.get("foreign_key").asString}.png"
-
-                setAndCacheMedia(media, year)
-                break
-            } else if (mediaType == CHIEF_DELPHI) {
-                media = "https://www.chiefdelphi.com/media/img/" + mediaObject.get("details")
-                        .asJsonObject
-                        .get("image_partial")
-                        .asString
-
-                setAndCacheMedia(media, year)
-                break
+            when (type) {
+                Imgur.ID -> Imgur("https://i.imgur.com/$key.png", preferred)
+                YouTube.ID ->
+                    YouTube("https://img.youtube.com/vi/$key/maxresdefault.jpg", preferred)
+                Instagram.ID ->
+                    Instagram("https://www.instagram.com/p/$key/media/?size=l", preferred)
+                ChiefDelphi.ID -> ChiefDelphi(
+                        "https://www.chiefdelphi.com/media/img/" + mediaObject.get("details")
+                                .asJsonObject
+                                .get("image_partial")
+                                .asString,
+                        preferred
+                )
+                else -> Unsupported(type)
             }
-        }
+        }.filterNot { it is Unsupported }.sorted().firstOrNull()
 
-        if (media.isNullOrBlank() && year > MAX_HISTORY) getTeamMedia(year - 1)
+        if (media != null) {
+            setAndCacheMedia(media.url, year)
+        } else if (year > MAX_HISTORY) {
+            getTeamMedia(year - 1)
+        }
     }
 
     private fun setAndCacheMedia(url: String, year: Int) {
@@ -89,11 +100,58 @@ class TbaDownloader private constructor(
         }
     }
 
+    private sealed class Media(val url: String, private val importance: Int) : Comparable<Media> {
+        override fun compareTo(other: Media) = other.importance.compareTo(importance)
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Media
+
+            return url == other.url
+        }
+
+        override fun hashCode() = url.hashCode()
+
+        override fun toString(): String {
+            return "${javaClass.simpleName}(url='$url')"
+        }
+
+        class Imgur(url: String, preferred: Boolean) :
+                Media(url, if (preferred) Int.MAX_VALUE else 0) {
+            companion object {
+                const val ID = "imgur"
+            }
+        }
+
+        class YouTube(url: String, preferred: Boolean) :
+                Media(url, (if (preferred) Int.MAX_VALUE else 0) - 1) {
+            companion object {
+                const val ID = "youtube"
+            }
+        }
+
+        class Instagram(url: String, preferred: Boolean) :
+                Media(url, (if (preferred) Int.MAX_VALUE else 0) - 2) {
+            companion object {
+                const val ID = "instagram-image"
+            }
+        }
+
+        class ChiefDelphi(url: String, preferred: Boolean) :
+                Media(url, (if (preferred) Int.MAX_VALUE else 0) - 3) {
+            companion object {
+                const val ID = "cdphotothread"
+            }
+        }
+
+        class Unsupported(type: String) : Media(type, Int.MIN_VALUE)
+    }
+
     companion object {
         private const val TEAM_NICKNAME = "nickname"
         private const val TEAM_WEBSITE = "website"
-        private const val IMGUR = "imgur"
-        private const val CHIEF_DELPHI = "cdphotothread"
         private const val MAX_HISTORY = 2000
 
         @WorkerThread
