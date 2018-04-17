SUPERCILEX/Robot-Scouter

commit c40eecf44b03613ec37e3c257f3513cacebb3990

Refactor app into different modules
Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

core-data/src/main/java/com/supercilex/robotscouter/core/data/model/Metrics.kt
@@ -1,0 +1,61 @@
+package com.supercilex.robotscouter.core.data.model
+
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.WriteBatch
+import com.supercilex.robotscouter.common.FIRESTORE_ID
+import com.supercilex.robotscouter.common.FIRESTORE_NAME
+import com.supercilex.robotscouter.common.FIRESTORE_SELECTED_VALUE_ID
+import com.supercilex.robotscouter.common.FIRESTORE_UNIT
+import com.supercilex.robotscouter.common.FIRESTORE_VALUE
+import com.supercilex.robotscouter.core.data.logUpdate
+import com.supercilex.robotscouter.core.logFailures
+import com.supercilex.robotscouter.core.model.Metric
+
+fun <T> Metric<T>.update(new: T) {
+    if (value != new) {
+        value = new
+        logUpdate()
+        ref.update(FIRESTORE_VALUE, new).logFailures(ref, new)
+    }
+}
+
+fun Metric<*>.updateName(new: String) {
+    if (name != new) {
+        name = new
+        ref.update(FIRESTORE_NAME, new).logFailures(ref, new)
+    }
+}
+
+fun Metric.Number.updateUnit(new: String?) {
+    if (unit != new) {
+        unit = new
+        ref.update(FIRESTORE_UNIT, new).logFailures(ref, new)
+    }
+}
+
+@Exclude
+fun Metric.List.update(items: List<Metric.List.Item>, batch: WriteBatch? = null) {
+    if (value != items) {
+        value = items
+        update(batch, FIRESTORE_VALUE, items.map {
+            mapOf(FIRESTORE_ID to it.id, FIRESTORE_NAME to it.name)
+        })
+    }
+}
+
+@Exclude
+fun Metric.List.updateSelectedValueId(new: String?, batch: WriteBatch? = null) {
+    if (selectedValueId != new) {
+        selectedValueId = new
+        logUpdate()
+        update(batch, FIRESTORE_SELECTED_VALUE_ID, new as Any)
+    }
+}
+
+private fun Metric.List.update(batch: WriteBatch?, id: String, any: Any) {
+    if (batch == null) {
+        ref.update(id, any).logFailures(ref, "Id: $id, update: $any")
+    } else {
+        batch.update(ref, id, any)
+    }
+}

feature-scouts/src/main/java/com/supercilex/robotscouter/feature/scouts/viewholder/EditTextViewHolder.kt
@@ -1,0 +1,48 @@
+package com.supercilex.robotscouter.feature.scouts.viewholder
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.view.View
+import com.supercilex.robotscouter.core.model.Metric
+import com.supercilex.robotscouter.core.unsafeLazy
+import com.supercilex.robotscouter.shared.scouting.MetricViewHolderBase
+import kotlinx.android.synthetic.main.scout_notes.*
+import java.util.Locale
+
+internal class EditTextViewHolder(
+        itemView: View
+) : MetricViewHolderBase<Metric.Text, String?>(itemView),
+        View.OnFocusChangeListener {
+    init {
+        name.onFocusChangeListener = this
+    }
+
+    /**
+     * Note: this implementation DOES NOT call super
+     */
+    // We set the text to the value instead of the name because the name goes in the hint
+    @SuppressLint("MissingSuperCall")
+    public override fun bind() {
+        textLayout.isHintAnimationEnabled = false
+        name.text = metric.value
+        textLayout.hint = metric.name
+        textLayout.isHintAnimationEnabled = true
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+                && metric.name.toUpperCase(Locale.ROOT).contains(nameHint)) {
+            itemView.importantForAutofill = View.IMPORTANT_FOR_AUTOFILL_AUTO
+            name.importantForAutofill = View.IMPORTANT_FOR_AUTOFILL_YES
+            name.setAutofillHints(View.AUTOFILL_HINT_NAME)
+        }
+    }
+
+    override fun onFocusChange(v: View, hasFocus: Boolean) {
+        if (!hasFocus) metric.value = name.text.toString()
+    }
+
+    private companion object {
+        @get:RequiresApi(Build.VERSION_CODES.O)
+        val nameHint by unsafeLazy { View.AUTOFILL_HINT_NAME.toUpperCase() }
+    }
+}

feature-scouts/src/main/java/com/supercilex/robotscouter/feature/scouts/viewholder/SpinnerViewHolder.kt
@@ -1,0 +1,47 @@
+package com.supercilex.robotscouter.feature.scouts.viewholder
+
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.supercilex.robotscouter.core.model.Metric
+import com.supercilex.robotscouter.feature.scouts.R
+import com.supercilex.robotscouter.shared.scouting.MetricViewHolderBase
+import kotlinx.android.synthetic.main.scout_spinner.*
+
+internal class SpinnerViewHolder(
+        itemView: View
+) : MetricViewHolderBase<Metric.List, List<Metric.List.Item>>(itemView),
+        AdapterView.OnItemSelectedListener {
+    init {
+        spinner.adapter = ArrayAdapter<String>(
+                itemView.context,
+                android.R.layout.simple_spinner_item
+        ).apply {
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        spinner.setBackgroundResource(R.drawable.abc_spinner_mtrl_am_alpha)
+        spinner.onItemSelectedListener = this
+    }
+
+    public override fun bind() {
+        super.bind()
+        updateAdapter()
+        spinner.setSelection(metric.selectedValueId?.let { id ->
+            metric.value.indexOfFirst { it.id == id }
+        } ?: 0)
+    }
+
+    private fun updateAdapter() {
+        @Suppress("UNCHECKED_CAST") // We know the metric type
+        (spinner.adapter as ArrayAdapter<String>).apply {
+            clear()
+            addAll(metric.value.map { it.name })
+        }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View, itemPosition: Int, id: Long) {
+        metric.selectedValueId = metric.value[itemPosition].id
+    }
+
+    override fun onNothingSelected(view: AdapterView<*>) = Unit
+}

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/CheckboxTemplateViewHolder.kt
@@ -1,0 +1,35 @@
+package com.supercilex.robotscouter.feature.templates.viewholder
+
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageView
+import com.supercilex.robotscouter.core.model.Metric
+import com.supercilex.robotscouter.core.unsafeLazy
+import com.supercilex.robotscouter.feature.templates.R
+import com.supercilex.robotscouter.shared.scouting.viewholder.CheckboxViewHolder
+import kotlinx.android.synthetic.main.scout_template_base_reorder.*
+import org.jetbrains.anko.find
+
+internal class CheckboxTemplateViewHolder(itemView: View) : CheckboxViewHolder(itemView),
+        MetricTemplateViewHolder<Metric.Boolean, Boolean> {
+    override val reorderView: ImageView by unsafeLazy { reorder }
+    override val nameEditor = name as EditText
+
+    private val checkBox = containerView.find<CheckBox>(R.id.checkBox)
+
+    init {
+        init()
+        name.setOnClickListener(null)
+    }
+
+    override fun bind() {
+        super.bind()
+        name.text = metric.name
+    }
+
+    override fun onClick(v: View) {
+        if (name.hasFocus()) metric.name = name.text.toString()
+        if (v.id == R.id.checkBox) metric.value = checkBox.isChecked
+    }
+}

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/CounterTemplateViewHolder.kt
@@ -1,0 +1,58 @@
+package com.supercilex.robotscouter.feature.templates.viewholder
+
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.updateLayoutParams
+import com.supercilex.robotscouter.core.data.nullOrFull
+import com.supercilex.robotscouter.core.model.Metric
+import com.supercilex.robotscouter.core.unsafeLazy
+import com.supercilex.robotscouter.feature.templates.R
+import com.supercilex.robotscouter.shared.scouting.viewholder.CounterViewHolder
+import kotlinx.android.synthetic.main.scout_template_base_reorder.*
+import kotlinx.android.synthetic.main.scout_template_counter.*
+import org.jetbrains.anko.find
+import java.util.Locale
+
+internal class CounterTemplateViewHolder(itemView: View) : CounterViewHolder(itemView),
+        MetricTemplateViewHolder<Metric.Number, Long> {
+    override val reorderView: ImageView by unsafeLazy { reorder }
+    override val nameEditor = name as EditText
+    override val valueWithoutUnit: String get() = count.text.toString()
+
+    private val count = containerView.find<TextView>(R.id.count)
+
+    init {
+        init()
+
+        itemView as LinearLayout
+        itemView.removeView(unit)
+        itemView.addView(unit, itemView.childCount - 1)
+        count.updateLayoutParams<LinearLayout.LayoutParams> { rightMargin = 0 }
+
+        unit.onFocusChangeListener = this
+    }
+
+    override fun bind() {
+        super.bind()
+        unit.setText(metric.unit)
+    }
+
+    override fun onClick(v: View) {
+        super.onClick(v)
+        if (name.hasFocus()) metric.name = name.text.toString()
+    }
+
+    override fun setValue() {
+        count.text = String.format(Locale.getDefault(), "%d", metric.value)
+    }
+
+    override fun onFocusChange(v: View, hasFocus: Boolean) {
+        super.onFocusChange(v, hasFocus)
+        if (!hasFocus && v === unit) {
+            metric.unit = unit.text.nullOrFull()?.toString()
+        }
+    }
+}

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/EditTextTemplateViewHolder.kt
@@ -1,0 +1,33 @@
+package com.supercilex.robotscouter.feature.templates.viewholder
+
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import com.supercilex.robotscouter.core.model.Metric
+import com.supercilex.robotscouter.core.unsafeLazy
+import com.supercilex.robotscouter.shared.scouting.MetricViewHolderBase
+import kotlinx.android.synthetic.main.scout_template_base_reorder.*
+import kotlinx.android.synthetic.main.scout_template_notes.*
+
+internal class EditTextTemplateViewHolder(
+        itemView: View
+) : MetricViewHolderBase<Metric.Text, String?>(itemView),
+        MetricTemplateViewHolder<Metric.Text, String?> {
+    override val reorderView: ImageView by unsafeLazy { reorder }
+    override val nameEditor = name as EditText
+
+    init {
+        init()
+        text.onFocusChangeListener = this
+    }
+
+    override fun bind() {
+        super.bind()
+        text.setText(metric.value)
+    }
+
+    override fun onFocusChange(v: View, hasFocus: Boolean) {
+        super.onFocusChange(v, hasFocus)
+        if (!hasFocus && v === text) metric.value = text.text.toString()
+    }
+}

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/MetricTemplateViewHolder.kt
@@ -1,0 +1,15 @@
+package com.supercilex.robotscouter.feature.templates.viewholder
+
+import android.view.View
+import com.supercilex.robotscouter.core.model.Metric
+
+internal interface MetricTemplateViewHolder<M : Metric<T>, T> : TemplateViewHolder {
+    var metric: M
+
+    override fun onFocusChange(v: View, hasFocus: Boolean) {
+        // Only save data when user is leaving metric
+        if (!hasFocus && v === nameEditor) {
+            metric.name = nameEditor.text.toString()
+        }
+    }
+}

commit af4f271a5169d043dc9ef289d322ddb325758304

Fix metrics not updating (regression introduced in c40eecf44b03613ec37e3c257f3513cacebb3990)
Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

core-data/src/main/java/com/supercilex/robotscouter/core/data/model/Metrics.kt
@@ -44,11 +44,11 @@ fun Metric.List.update(items: List<Metric.List.Item>, batch: WriteBatch? = null)
 }
 
 @Exclude
-fun Metric.List.updateSelectedValueId(new: String?, batch: WriteBatch? = null) {
+fun Metric.List.updateSelectedValueId(new: String?) {
     if (selectedValueId != new) {
         selectedValueId = new
         logUpdate()
-        update(batch, FIRESTORE_SELECTED_VALUE_ID, new as Any)
+        update(null, FIRESTORE_SELECTED_VALUE_ID, new as Any)
     }
 }
 

feature-scouts/src/main/java/com/supercilex/robotscouter/feature/scouts/viewholder/EditTextViewHolder.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Build
 import android.support.annotation.RequiresApi
 import android.view.View
+import com.supercilex.robotscouter.core.data.model.update
 import com.supercilex.robotscouter.core.model.Metric
 import com.supercilex.robotscouter.core.unsafeLazy
 import com.supercilex.robotscouter.shared.scouting.MetricViewHolderBase
@@ -38,7 +39,7 @@ internal class EditTextViewHolder(
     }
 
     override fun onFocusChange(v: View, hasFocus: Boolean) {
-        if (!hasFocus) metric.value = name.text.toString()
+        if (!hasFocus) metric.update(name.text.toString())
     }
 
     private companion object {

feature-scouts/src/main/java/com/supercilex/robotscouter/feature/scouts/viewholder/SpinnerViewHolder.kt
@@ -3,6 +3,7 @@ package com.supercilex.robotscouter.feature.scouts.viewholder
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import com.supercilex.robotscouter.core.data.model.updateSelectedValueId
 import com.supercilex.robotscouter.core.model.Metric
 import com.supercilex.robotscouter.feature.scouts.R
 import com.supercilex.robotscouter.shared.scouting.MetricViewHolderBase
@@ -40,7 +41,7 @@ internal class SpinnerViewHolder(
     }
 
     override fun onItemSelected(parent: AdapterView<*>, view: View, itemPosition: Int, id: Long) {
-        metric.selectedValueId = metric.value[itemPosition].id
+        metric.updateSelectedValueId(metric.value[itemPosition].id)
     }
 
     override fun onNothingSelected(view: AdapterView<*>) = Unit

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/CheckboxTemplateViewHolder.kt
@@ -4,6 +4,8 @@ import android.view.View
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageView
+import com.supercilex.robotscouter.core.data.model.update
+import com.supercilex.robotscouter.core.data.model.updateName
 import com.supercilex.robotscouter.core.model.Metric
 import com.supercilex.robotscouter.core.unsafeLazy
 import com.supercilex.robotscouter.feature.templates.R
@@ -29,7 +31,7 @@ internal class CheckboxTemplateViewHolder(itemView: View) : CheckboxViewHolder(i
     }
 
     override fun onClick(v: View) {
-        if (name.hasFocus()) metric.name = name.text.toString()
-        if (v.id == R.id.checkBox) metric.value = checkBox.isChecked
+        if (name.hasFocus()) metric.updateName(name.text.toString())
+        if (v.id == R.id.checkBox) metric.update(checkBox.isChecked)
     }
 }

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/CounterTemplateViewHolder.kt
@@ -6,6 +6,8 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.updateLayoutParams
+import com.supercilex.robotscouter.core.data.model.updateName
+import com.supercilex.robotscouter.core.data.model.updateUnit
 import com.supercilex.robotscouter.core.data.nullOrFull
 import com.supercilex.robotscouter.core.model.Metric
 import com.supercilex.robotscouter.core.unsafeLazy
@@ -42,7 +44,7 @@ internal class CounterTemplateViewHolder(itemView: View) : CounterViewHolder(ite
 
     override fun onClick(v: View) {
         super.onClick(v)
-        if (name.hasFocus()) metric.name = name.text.toString()
+        if (name.hasFocus()) metric.updateName(name.text.toString())
     }
 
     override fun setValue() {
@@ -52,7 +54,7 @@ internal class CounterTemplateViewHolder(itemView: View) : CounterViewHolder(ite
     override fun onFocusChange(v: View, hasFocus: Boolean) {
         super.onFocusChange(v, hasFocus)
         if (!hasFocus && v === unit) {
-            metric.unit = unit.text.nullOrFull()?.toString()
+            metric.updateUnit(unit.text.nullOrFull()?.toString())
         }
     }
 }

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/EditTextTemplateViewHolder.kt
@@ -3,6 +3,7 @@ package com.supercilex.robotscouter.feature.templates.viewholder
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import com.supercilex.robotscouter.core.data.model.update
 import com.supercilex.robotscouter.core.model.Metric
 import com.supercilex.robotscouter.core.unsafeLazy
 import com.supercilex.robotscouter.shared.scouting.MetricViewHolderBase
@@ -28,6 +29,6 @@ internal class EditTextTemplateViewHolder(
 
     override fun onFocusChange(v: View, hasFocus: Boolean) {
         super.onFocusChange(v, hasFocus)
-        if (!hasFocus && v === text) metric.value = text.text.toString()
+        if (!hasFocus && v === text) metric.update(text.text.toString())
     }
 }

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/MetricTemplateViewHolder.kt
@@ -1,6 +1,7 @@
 package com.supercilex.robotscouter.feature.templates.viewholder
 
 import android.view.View
+import com.supercilex.robotscouter.core.data.model.updateName
 import com.supercilex.robotscouter.core.model.Metric
 
 internal interface MetricTemplateViewHolder<M : Metric<T>, T> : TemplateViewHolder {
@@ -9,7 +10,7 @@ internal interface MetricTemplateViewHolder<M : Metric<T>, T> : TemplateViewHold
     override fun onFocusChange(v: View, hasFocus: Boolean) {
         // Only save data when user is leaving metric
         if (!hasFocus && v === nameEditor) {
-            metric.name = nameEditor.text.toString()
+            metric.updateName(nameEditor.text.toString())
         }
     }
 }

feature-templates/src/main/java/com/supercilex/robotscouter/feature/templates/viewholder/SpinnerTemplateViewHolder.kt
@@ -65,10 +65,10 @@ internal class SpinnerTemplateViewHolder(
 
     override fun onClick(v: View) {
         val position = metric.value.size
-        metric.value = mutableListOf(
+        metric.update(mutableListOf(
                 *getLatestItems().toTypedArray(),
                 Metric.List.Item(metric.ref.parent.document().id, "")
-        )
+        ))
         itemTouchCallback.pendingScrollPosition = position
         items.adapter.notifyItemInserted(position)
     }
@@ -145,7 +145,7 @@ internal class SpinnerTemplateViewHolder(
             val metric = parent.metric
             val oldDefaultId = metric.selectedValueId
             firestoreBatch {
-                metric.updateSelectedValueId(item.id, this)
+                metric.updateSelectedValueId(item.id)
                 metric.update(items, this)
             }.logFailures()
             parent.items.notifyItemsNoChangeAnimation {
@@ -160,15 +160,15 @@ internal class SpinnerTemplateViewHolder(
 
         private fun delete(items: List<Metric.List.Item>) {
             val position = items.indexOfFirst { it.id == item.id }
-            parent.metric.value = items.toMutableList().apply {
+            parent.metric.update(items.toMutableList().apply {
                 removeAt(position)
-            }
+            })
             parent.items.adapter.notifyItemRemoved(position)
 
             longSnackbar(itemView, R.string.deleted, R.string.undo) {
-                parent.metric.value = parent.metric.value.toMutableList().apply {
+                parent.metric.update(parent.metric.value.toMutableList().apply {
                     add(position, items[position])
-                }
+                })
                 parent.items.adapter.notifyItemInserted(position)
             }
         }
@@ -179,7 +179,7 @@ internal class SpinnerTemplateViewHolder(
                 !hasFocus && v === nameEditor && adapterPosition != -1 &&
                 metric.value.find { it.id == item.id } != null
             ) {
-                metric.value = getUpdatedItems(metric.value)
+                metric.update(getUpdatedItems(metric.value))
             }
         }
 
@@ -233,7 +233,7 @@ internal class SpinnerTemplateViewHolder(
             super.clearView(recyclerView, viewHolder)
             items.setHasFixedSize(false)
             localItems?.let {
-                metric.value = it
+                metric.update(it)
                 localItems = null
             }
         }

shared-scouting/src/main/java/com/supercilex/robotscouter/shared/scouting/viewholder/CheckboxViewHolder.kt
@@ -1,6 +1,7 @@
 package com.supercilex.robotscouter.shared.scouting.viewholder
 
 import android.view.View
+import com.supercilex.robotscouter.core.data.model.update
 import com.supercilex.robotscouter.core.model.Metric
 import com.supercilex.robotscouter.shared.scouting.MetricViewHolderBase
 import com.supercilex.robotscouter.shared.scouting.R
@@ -22,7 +23,7 @@ open class CheckboxViewHolder(
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.checkBox) metric.value = checkBox.isChecked
+        if (v.id == R.id.checkBox) metric.update(checkBox.isChecked)
         if (v.id == R.id.name) checkBox.performClick()
     }
 }

shared-scouting/src/main/java/com/supercilex/robotscouter/shared/scouting/viewholder/CounterViewHolder.kt
@@ -4,6 +4,7 @@ import android.support.annotation.CallSuper
 import android.support.transition.TransitionManager
 import android.view.View
 import android.view.ViewGroup
+import com.supercilex.robotscouter.core.data.model.update
 import com.supercilex.robotscouter.core.model.Metric
 import com.supercilex.robotscouter.core.ui.setOnLongClickListenerCompat
 import com.supercilex.robotscouter.shared.scouting.CounterValueDialog
@@ -44,9 +45,9 @@ open class CounterViewHolder(
         var value = valueWithoutUnit.toLong()
 
         if (id == R.id.increment) {
-            metric.value = ++value
+            metric.update(++value)
         } else if (id == R.id.decrement) {
-            metric.value = --value
+            metric.update(--value)
         }
 
         TransitionManager.beginDelayedTransition(itemView as ViewGroup)

shared-scouting/src/main/java/com/supercilex/robotscouter/shared/scouting/viewholder/StopwatchViewHolder.kt
@@ -18,6 +18,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.github.rubensousa.gravitysnaphelper.GravitySnapHelper
 import com.supercilex.robotscouter.core.RobotScouter
+import com.supercilex.robotscouter.core.data.model.update
 import com.supercilex.robotscouter.core.data.second
 import com.supercilex.robotscouter.core.model.Metric
 import com.supercilex.robotscouter.core.reportOrCancel
@@ -86,14 +87,14 @@ open class StopwatchViewHolder(
             timer = Timer(this)
         } else {
             val lap = currentTimer.cancel()
-            metric.value = metric.value.toMutableList().apply { add(lap) }
+            metric.update(metric.value.toMutableList().apply { add(lap) })
 
             metric.value.size.let { notifyCycleAdded(it, it) }
 
             longSnackbar(itemView, R.string.scout_stopwatch_lap_added_message, R.string.undo) {
                 val hadAverage = metric.value.size >= LIST_SIZE_WITH_AVERAGE
                 val newCycles = metric.value.toMutableList().apply { remove(lap) }
-                metric.value = newCycles
+                metric.update(newCycles)
 
                 notifyCycleRemoved(metric.value.size, metric.value.size, hadAverage)
                 timer = Timer(this, currentTimer.startTimeMillis)
@@ -323,16 +324,16 @@ open class StopwatchViewHolder(
 
             val newCycles = metric.value.toMutableList()
             val deletedCycle = newCycles.removeAt(position)
-            metric.value = newCycles
+            metric.update(newCycles)
 
             holder.notifyCycleRemoved(position, metric.value.size, hadAverage)
 
             longSnackbar(itemView, R.string.deleted, R.string.undo) {
                 val latestMetric = holder.metric
 
-                latestMetric.value = latestMetric.value.toMutableList().apply {
+                latestMetric.update(latestMetric.value.toMutableList().apply {
                     add(position, deletedCycle)
-                }
+                })
 
                 holder.notifyCycleAdded(rawPosition, latestMetric.value.size)
             }