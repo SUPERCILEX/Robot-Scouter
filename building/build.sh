717eb2a03ad97484fb8a0678aecb0fbe5c63de5b
Alex Saveau <saveau.alexandre@gmail.com>
2018-05-17 17:12:21 -0700

    Fix broken default template deployment
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- building/build.sh
+++ building/build.sh
@@ -4,6 +4,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
   ./gradlew clean build publishApkRelease
 
   building/deploy-firebase.sh
+  sleep 30 # Wait long enough to circumvent function `finished with status: 'connection error'`
   ./google-cloud-sdk/bin/gcloud alpha pubsub topics publish update-default-templates --message '{}'
 else
   ./gradlew clean assembleDebug check
