f9f1908404e1d20e2541ea5adcadf2269707429b
Alex Saveau <saveau.alexandre@gmail.com>
2018-05-25 15:51:23 -0700

    Re-disable release builds on public repo
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- building/build.sh
+++ building/build.sh
@@ -3,11 +3,13 @@
 ./gradlew clean
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
-  ./gradlew build publishApkRelease --no-parallel
+./gradlew assembleDebug check --no-parallel
 
-  building/deploy-firebase.sh
-  sleep 30 # Wait long enough to circumvent function `finished with status: 'connection error'`
-  ./google-cloud-sdk/bin/gcloud alpha pubsub topics publish update-default-templates --message '{}'
+#  ./gradlew build publishApkRelease --no-parallel
+#
+#  building/deploy-firebase.sh
+#  sleep 30 # Wait long enough to circumvent function `finished with status: 'connection error'`
+#  ./google-cloud-sdk/bin/gcloud alpha pubsub topics publish update-default-templates --message '{}'
 else
   ./gradlew assembleDebug check --no-parallel
 fi
