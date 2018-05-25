8778face4429c61bf2cdba6cc005ac72cd35c600
Alex Saveau <saveau.alexandre@gmail.com>
2018-05-24 19:25:56 -0700

    Fix Travis builds randomly breaking
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- building/build.sh
+++ building/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
+./gradlew clean
+
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
-  ./gradlew clean build publishApkRelease
+  ./gradlew build publishApkRelease --no-daemon
 
   building/deploy-firebase.sh
   sleep 30 # Wait long enough to circumvent function `finished with status: 'connection error'`
   ./google-cloud-sdk/bin/gcloud alpha pubsub topics publish update-default-templates --message '{}'
 else
-  ./gradlew clean assembleDebug check
+  ./gradlew assembleDebug check --no-daemon
 fi
