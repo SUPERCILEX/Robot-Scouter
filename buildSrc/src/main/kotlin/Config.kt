b669df26ce4605bdd1c48e2523db94bdb415d2dd
Alex Saveau <saveau.alexandre@gmail.com>
2018-06-25 19:21:41 -0700

    Remove const modifiers to improve build perf (closes #235)
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,103 +1,104 @@
+@Suppress("MayBeConstant") // Improve perf when changing values
 object Config {
     private const val kotlinVersion = "1.2.50"
 
     object SdkVersions {
-        const val compile = 28
-        const val target = 28
-        const val min = 16
+        val compile = 28
+        val target = 28
+        val min = 16
     }
 
     object Plugins {
-        const val android = "com.android.tools.build:gradle:3.2.0-alpha18"
-        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+        val android = "com.android.tools.build:gradle:3.2.0-alpha18"
+        val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
 
-        const val google = "com.google.gms:google-services:3.2.1"
-        const val firebase = "com.google.firebase:firebase-plugins:1.1.5"
-        const val fabric = "io.fabric.tools:gradle:1.25.4"
-        const val publishing = "com.github.Triple-T:gradle-play-publisher:fab6d986c1"
+        val google = "com.google.gms:google-services:3.2.1"
+        val firebase = "com.google.firebase:firebase-plugins:1.1.5"
+        val fabric = "io.fabric.tools:gradle:1.25.4"
+        val publishing = "com.github.Triple-T:gradle-play-publisher:fab6d986c1"
 
-        const val ktlint = "com.github.shyiko:ktlint:0.23.1"
+        val ktlint = "com.github.shyiko:ktlint:0.23.1"
     }
 
     object Libs {
         object Kotlin {
             private const val coroutinesVersion = "0.23.3"
 
-            const val jvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
-            const val js = "org.jetbrains.kotlin:kotlin-stdlib-js:$kotlinVersion"
-            const val coroutinesAndroid =
+            val jvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
+            val js = "org.jetbrains.kotlin:kotlin-stdlib-js:$kotlinVersion"
+            val coroutinesAndroid =
                     "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
-            const val coroutinesJs =
+            val coroutinesJs =
                     "org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$coroutinesVersion"
-            const val ktx = "androidx.core:core-ktx:0.3"
+            val ktx = "androidx.core:core-ktx:0.3"
         }
 
         object Anko {
             private const val version = "0.10.5"
 
-            const val common = "org.jetbrains.anko:anko-common:$version"
-            const val coroutines = "org.jetbrains.anko:anko-coroutines:$version"
-            const val appCompat = "org.jetbrains.anko:anko-appcompat-v7-commons:$version"
-            const val design = "org.jetbrains.anko:anko-design:$version"
+            val common = "org.jetbrains.anko:anko-common:$version"
+            val coroutines = "org.jetbrains.anko:anko-coroutines:$version"
+            val appCompat = "org.jetbrains.anko:anko-appcompat-v7-commons:$version"
+            val design = "org.jetbrains.anko:anko-design:$version"
         }
 
         object Support {
             private const val version = "27.1.1"
 
-            const val multidex = "com.android.support:multidex:1.0.3"
-            const val v4 = "com.android.support:support-v4:$version"
-            const val appCompat = "com.android.support:appcompat-v7:$version"
-            const val design = "com.android.support:design:$version"
-            const val palette = "com.android.support:palette-v7:$version"
-            const val cardView = "com.android.support:cardview-v7:$version"
-            const val emoji = "com.android.support:support-emoji-appcompat:$version"
-            const val customTabs = "com.android.support:support-emoji-appcompat:$version"
-            const val pref = "com.android.support:preference-v7:$version"
+            val multidex = "com.android.support:multidex:1.0.3"
+            val v4 = "com.android.support:support-v4:$version"
+            val appCompat = "com.android.support:appcompat-v7:$version"
+            val design = "com.android.support:design:$version"
+            val palette = "com.android.support:palette-v7:$version"
+            val cardView = "com.android.support:cardview-v7:$version"
+            val emoji = "com.android.support:support-emoji-appcompat:$version"
+            val customTabs = "com.android.support:support-emoji-appcompat:$version"
+            val pref = "com.android.support:preference-v7:$version"
 
-            const val constraint = "com.android.support.constraint:constraint-layout:1.1.2"
+            val constraint = "com.android.support.constraint:constraint-layout:1.1.2"
         }
 
         object Arch {
             private const val version = "1.1.1"
             private const val workVersion = "1.0.0-alpha02"
 
-            const val common = "android.arch.lifecycle:common-java8:$version"
-            const val extensions = "android.arch.lifecycle:extensions:$version"
-            const val work = "android.arch.work:work-runtime-ktx:$workVersion"
-            const val workFirebase = "android.arch.work:work-firebase:$workVersion"
+            val common = "android.arch.lifecycle:common-java8:$version"
+            val extensions = "android.arch.lifecycle:extensions:$version"
+            val work = "android.arch.work:work-runtime-ktx:$workVersion"
+            val workFirebase = "android.arch.work:work-firebase:$workVersion"
         }
 
         object Firebase {
-            const val core = "com.google.firebase:firebase-core:16.0.0"
-            const val auth = "com.google.firebase:firebase-auth:16.0.1"
-            const val firestore = "com.google.firebase:firebase-firestore:17.0.1"
-            const val functions = "com.google.firebase:firebase-functions:16.0.1"
-            const val storage = "com.google.firebase:firebase-storage:16.0.1"
-            const val config = "com.google.firebase:firebase-config:16.0.0"
-            const val indexing = "com.google.firebase:firebase-appindexing:15.0.1"
-            const val messaging = "com.google.firebase:firebase-messaging:17.0.0"
-            const val invites = "com.google.firebase:firebase-invites:16.0.0"
-            const val perf = "com.google.firebase:firebase-perf:16.0.0"
+            val core = "com.google.firebase:firebase-core:16.0.0"
+            val auth = "com.google.firebase:firebase-auth:16.0.1"
+            val firestore = "com.google.firebase:firebase-firestore:17.0.1"
+            val functions = "com.google.firebase:firebase-functions:16.0.1"
+            val storage = "com.google.firebase:firebase-storage:16.0.1"
+            val config = "com.google.firebase:firebase-config:16.0.0"
+            val indexing = "com.google.firebase:firebase-appindexing:15.0.1"
+            val messaging = "com.google.firebase:firebase-messaging:17.0.0"
+            val invites = "com.google.firebase:firebase-invites:16.0.0"
+            val perf = "com.google.firebase:firebase-perf:16.0.0"
 
-            const val crashlytics = "com.crashlytics.sdk.android:crashlytics:2.9.2"
+            val crashlytics = "com.crashlytics.sdk.android:crashlytics:2.9.2"
         }
 
         object PlayServices {
             private const val version = "15.0.1"
 
-            const val auth = "com.google.android.gms:play-services-auth:$version"
-            const val nearby = "com.google.android.gms:play-services-nearby:$version"
-            const val playCore = "com.google.android.play:core:1.2.0"
+            val auth = "com.google.android.gms:play-services-auth:$version"
+            val nearby = "com.google.android.gms:play-services-nearby:$version"
+            val playCore = "com.google.android.play:core:1.2.0"
         }
 
         object FirebaseUi {
             private const val version = "c280098444"
 
-            const val firestore =
+            val firestore =
                     "com.github.SUPERCILEX.FirebaseUI-Android:firebase-ui-firestore:$version"
-            const val auth = "com.github.SUPERCILEX.FirebaseUI-Android:firebase-ui-auth:$version"
-            const val facebook = "com.facebook.android:facebook-login:4.33.0"
-            const val twitter = "com.twitter.sdk.android:twitter-core:3.1.1@aar"
+            val auth = "com.github.SUPERCILEX.FirebaseUI-Android:firebase-ui-auth:$version"
+            val facebook = "com.facebook.android:facebook-login:4.33.0"
+            val twitter = "com.twitter.sdk.android:twitter-core:3.1.1@aar"
         }
 
         object Misc {
@@ -107,22 +108,22 @@ object Config {
 
             private const val glideVersion = "4.7.1"
 
-            const val leakCanary = "com.squareup.leakcanary:leakcanary-android:$leakCanaryVersion"
-            const val leakCanaryNoop =
+            val leakCanary = "com.squareup.leakcanary:leakcanary-android:$leakCanaryVersion"
+            val leakCanaryNoop =
                     "com.squareup.leakcanary:leakcanary-android-no-op:$leakCanaryVersion"
-            const val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
-            const val retrofitGson = "com.squareup.retrofit2:converter-gson:$retrofitVersion"
-            const val gson = "com.google.code.gson:gson:2.8.4"
-            const val poi = "com.github.SUPERCILEX.poi-android:poi:$poiVersion"
-            const val poiProguard = "com.github.SUPERCILEX.poi-android:proguard:$poiVersion"
+            val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+            val retrofitGson = "com.squareup.retrofit2:converter-gson:$retrofitVersion"
+            val gson = "com.google.code.gson:gson:2.8.4"
+            val poi = "com.github.SUPERCILEX.poi-android:poi:$poiVersion"
+            val poiProguard = "com.github.SUPERCILEX.poi-android:proguard:$poiVersion"
 
-            const val glide = "com.github.bumptech.glide:glide:$glideVersion"
-            const val glideRv = "com.github.bumptech.glide:recyclerview-integration:$glideVersion"
-            const val snap = "com.github.rubensousa:gravitysnaphelper:1.5"
-            const val permissions = "pub.devrel:easypermissions:1.2.0"
-            const val mttp = "uk.co.samuelwall:material-tap-target-prompt:2.9.0"
-            const val billing = "com.android.billingclient:billing:1.1"
-            const val licenses = "net.yslibrary.licenseadapter:licenseadapter:2.0.2"
+            val glide = "com.github.bumptech.glide:glide:$glideVersion"
+            val glideRv = "com.github.bumptech.glide:recyclerview-integration:$glideVersion"
+            val snap = "com.github.rubensousa:gravitysnaphelper:1.5"
+            val permissions = "pub.devrel:easypermissions:1.2.0"
+            val mttp = "uk.co.samuelwall:material-tap-target-prompt:2.9.0"
+            val billing = "com.android.billingclient:billing:1.1"
+            val licenses = "net.yslibrary.licenseadapter:licenseadapter:2.0.2"
         }
     }
 }
