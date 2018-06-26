fa189370c1cb34d2d240846a96e114aaff37daa9
Alex Saveau <saveau.alexandre@gmail.com>
2018-06-25 19:45:07 -0700

    Revamp community files (closes #236)
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- .buildscript/build.sh
+++ .buildscript/build.sh
@@ -11,7 +11,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
 
 #  ./gradlew build publishApkRelease --no-parallel
 #
-#  building/deploy-firebase.sh
+#  .buildscript/deploy-firebase.sh
 #  sleep 30 # Wait long enough to circumvent function `finished with status: 'connection error'`
 #  ./google-cloud-sdk/bin/gcloud alpha pubsub topics publish update-default-templates --message '{}'
 else
--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,8 +0,0 @@
-<!--
-Welcome to Robot Scouter and thanks for submitting an issue!
-
-If this isn't a
-[bug report](https://github.com/SUPERCILEX/Robot-Scouter/issues/new?template=bugs.md) or a
-[feature request](https://github.com/SUPERCILEX/Robot-Scouter/issues/new?template=features.md), feel
-free to share your feedback here.
--->
