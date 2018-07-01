fa91bbb2bd6a961c63a5a93946a8eb2636598b7d
Alex Saveau <saveau.alexandre@gmail.com>
2018-06-30 21:18:37 -0700

    Fix isOffline being wrong if network adapters are turned off
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- library/core/src/main/java/com/supercilex/robotscouter/core/Connectivity.kt
+++ library/core/src/main/java/com/supercilex/robotscouter/core/Connectivity.kt
@@ -3,9 +3,9 @@ package com.supercilex.robotscouter.core
 import android.content.Context
 import android.net.ConnectivityManager
 
-val isOnline get() = !isOffline
+val isOnline get() = connectivityManager.activeNetworkInfo?.isConnected == true
 
-val isOffline get() = connectivityManager.activeNetworkInfo?.isConnected == false
+val isOffline get() = !isOnline
 
 private val connectivityManager by lazy {
     RobotScouter.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
